eb5ea58453237aa0758a0bdee686d339582ca2f7
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-03-27 17:57:47 +0000

    added shells for random

---
+++ trunk/src/edu/udel/cis/cisc475/rex/random/randomizer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author  team 5
+ *
+ */
+public class randomizer implements randomizerIF {
+
+	
+	/* General purpose random chooser: 
+	 * can be used to permute answers 
+	 * (let numItems=items.length) 
+	 * permute topics, select problems 
+	 * satisfying constraints (first filter then select) */
+public Object[]  choose (int numItems, Object[] items) {
+
+	
+	
+     }//end of choose method
+}//end of class 
+	   
+
+}
---
+++ trunk/src/edu/udel/cis/cisc475/rex/random/randomizerIF.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author  team 5
+ *
+ */
+/* General purpose random chooser: 
+ * can be used to permute answers 
+ * (let numItems=items.length) 
+ * permute topics, select problems 
+ * satisfying constraints (first filter then select) */
+public interface randomizerIF {
+     Object[]  choose(int numItems, Object[]  items);
+   
+}
