9a40a1d683f2b7e49f4cb5bd53f147416c938e4f
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-03-27 17:36:11 +0000

    fixed sourceIF calling and bounds for write method

--- src/edu/udel/cis/cisc475/rex/source/SourceFactoryIF.java
+++ src/edu/udel/cis/cisc475/rex/source/SourceFactoryIF.java
@@ -1,4 +1,4 @@
-package edu.udel.cis.cisc475.rex.source.IF;
+//package edu.udel.cis.cisc475.rex.source.IF;
 
 /**
  * 
@@ -18,9 +18,14 @@ public class SourceFactoryIF {
 /**
  * factory method returns an object that implements SourceIF interface.
  */
-	public SourceIF newSource(String filename){
-		Source S = new Source(filename);  
-		return S;
+//	public SourceIF newSource(String filename){
+		//declare a pointer to an interface
+//		SourceIF S = new SourceIF;
+
+		//create an object of the interface type
+//		S = new Source(filename);
+
+//		return S;
 	
 	}//end of newSource
 }//end of class SourceFactoryIF 
