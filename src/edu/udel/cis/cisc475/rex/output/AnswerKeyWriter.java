870615cd02f10050998875c65ae18cd98c8e6dd6
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-02 17:11:35 +0000

    added shells for output module, answer key writer works.  others are not yet built

---
+++ src/edu/udel/cis/cisc475/rex/output/AnswerKeyWriter.java
@@ -0,0 +1,48 @@
+package IF;
+//package edu.udel.cis.cisc475.rex.output.IF;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 
+
+
+/**
+ * @author team 5
+ *
+ */
+public class AnswerKeyWriter implements AnswerKeyWriterIF {
+
+	//constructor
+	public AnswerKeyWriter (Key K){
+	this.K = K;
+	}//end of constructor
+	
+	public void write(PrintWriter out){
+		out.printf("Exam version : \t %s\n", K.version() ); 
+		out.printf("Exam Name : \t %s\n", K.examName() );
+		out.printf("Date :      \t %s\n", K.date() ); 
+		out.printf("\nThis Exam contains %d problems. \n", K.numProblems() ); 
+
+		C = new ArrayList<String> (K.answers(0) );
+		int count = 1;
+		out.printf("\n\t  Answer Key  \n"); 
+
+		for (String c: C) {
+	 		out.printf("problem %d :   %s\n", count, c); 
+	 		count++;
+	 	}
+}//end of write(PrintWriter out)
+
+	
+	
+public void writeScantron(File file){
+
+
+}//end of writeScantron(File file)
+
+//private vars
+private Key K;
+private ArrayList <String> C;	
+}//end of class
---
+++ src/edu/udel/cis/cisc475/rex/output/ExamWriter.java
@@ -0,0 +1,21 @@
+package IF;
+//package edu.udel.cis.cisc475.rex.source;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author team 5
+ *
+ */
+public class ExamWriter implements ExamWriterIF {
+
+
+//constructor
+public ExamWriter() {
+}//end of constructor
+
+public void write(PrintWriter out){
+}//end of write(PrintWriter out)
+	
+}//end of class 
