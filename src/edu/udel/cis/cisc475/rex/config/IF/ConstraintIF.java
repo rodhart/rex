0e11724ac2b44bd7f879b4fe0b81f3365b0a2b0e
aplatt <aplatt@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-06 01:42:00 +0000

    added RequiredProblemConstraintIF and GroupConstraintIF and ConstraintIF

---
+++ src/edu/udel/cis/cisc475/rex/config/IF/ConstraintIF.java
@@ -0,0 +1,15 @@
+//package edu.udel.cis.cisc475.rex.config;
+
+import java.util.*;
+import java.io.*;
+
+
+/*
+ * @author team 2
+ *
+ */
+
+public interface ConstraintIF {
+    SourceIF source();
+
+}//end of interface
---
+++ src/edu/udel/cis/cisc475/rex/config/IF/RequiredProblemConstraintIF.java
@@ -0,0 +1,16 @@
+//package edu.udel.cis.cisc475.rex.config;
+
+import java.util.*;
+import java.io.*;
+
+
+/*
+ * @author team 2
+ *
+ */
+
+public interface RequiredProblemConstraintIF {
+    String label();
+    int points();
+
+}//end of interface
