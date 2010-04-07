25309967459596e0f0f7d10377ae99c531cdc799
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-07 20:28:59 +0000

    Adding skeleton for AnswerKeyFactory and static method for creating one as example.

---
+++ trunk/src/edu/udel/cis/cisc475/rex/key/impl/AnswerKeyFactory.java
@@ -0,0 +1,14 @@
+package edu.udel.cis.cisc475.rex.key.impl;
+
+import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyFactoryIF;
+import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
+
+public class AnswerKeyFactory implements AnswerKeyFactoryIF {
+
+	@Override
+	public AnswerKeyIF newAnswerKey(String version, String examName, String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
