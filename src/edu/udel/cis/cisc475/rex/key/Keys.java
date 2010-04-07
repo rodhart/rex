4b1c08656b1b0d33bdf0fe793cd3fee88618fc42
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-07 20:28:59 +0000

    Adding skeleton for AnswerKeyFactory and static method for creating one as example.

--- src/edu/udel/cis/cisc475/rex/key/Keys.java
+++ src/edu/udel/cis/cisc475/rex/key/Keys.java
@@ -1,6 +1,7 @@
 package edu.udel.cis.cisc475.rex.key;
 
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyFactoryIF;
+import edu.udel.cis.cisc475.rex.key.impl.AnswerKeyFactory;
 
 public class Keys {
 
