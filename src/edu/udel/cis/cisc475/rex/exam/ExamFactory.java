5dd2cd6c454fda106fecfbe2cacd3ce735dbec2e
martz <martz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-07 16:47:27 +0000

    Added default ExamFactory.java implementation of ExamFactoryIF.

---
+++ src/edu/udel/cis/cisc475/rex/exam/ExamFactory.java
@@ -0,0 +1,59 @@
+package edu.udel.cis.cisc475.rex.exam;
+
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FixedAnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+
+public class ExamFactory implements ExamFactoryIF {
+
+	@Override
+	public AnswerIF newAnswer(boolean correct, SourceIF text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BlockIF newBlock(String topic, String label, SourceIF text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FigureIF newFigure(String label, SourceIF text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FixedAnswerIF newFixedAnswer(boolean correct, int index,
+			SourceIF text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ExamIF newGeneratedExam() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ExamIF newMasterExam() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProblemIF newProblem(String topic, String label, SourceIF question,
+			AnswerIF[] answers) {
+		// TODO Auto-generated method stub
+	
+		return null;
+	}
+
+}
