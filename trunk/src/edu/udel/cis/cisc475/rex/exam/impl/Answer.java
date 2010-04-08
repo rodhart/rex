f1982f0d67faefc5e0ac7221ca4fe56c3985df37
fitzpatr <fitzpatr@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-08 03:14:58 +0000

    Created the exam/impl and moved Exam and ExamFactory files into it. Added Answer, Block, ExamElement,Figure,FixedAnswer, Problem skeleton files.

---
+++ trunk/src/edu/udel/cis/cisc475/rex/exam/impl/Answer.java
@@ -0,0 +1,20 @@
+package edu.udel.cis.cisc475.rex.exam.impl;
+
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+
+public class Answer implements AnswerIF {
+
+	@Override
+	public boolean isCorrect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SourceIF source() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
