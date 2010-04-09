71819261c94965fa4e03e64c5e8d1f79475d7114
boyd <boyd@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-09 22:16:10 +0000

    Implemented v1 of AnswerTest methods

--- test-exam/edu/udel/cis/cisc475/rex/exam/AnswerTest.java
+++ test-exam/edu/udel/cis/cisc475/rex/exam/AnswerTest.java
@@ -1,12 +1,18 @@
 /**
  * rex
  * edu.udel.cis.cisc475.rex.exam
+ * Test Suite for testing Answer methods.
  * Apr 9, 2010
  * hboyd
  */
 package edu.udel.cis.cisc475.rex.exam;
 
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.lang.reflect.Array;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,16 +21,55 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
+import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.examstubs.SourceFactoryStub;
+
 
 /**
  * @author hboyd
  *
  */
 public class AnswerTest {
-
+	public final static boolean useStubs = true;
+	
+	private static SourceFactoryIF sourceFactory;
+	private static ExamFactoryIF examFactory;
+	
+	private static SourceIF answerSource1;
+	private static SourceIF answerSource2;
+	private static AnswerIF answer1;
+	private static AnswerIF answer2;
+	
+	private static String testUEFfilename = "testFileName.txt";
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	
+		if (useStubs) {
+			sourceFactory = new SourceFactoryStub();
+		}
+		else{
+			// TODO Uncomment when entry point is available
+			//sourceFactory = Sources.newSourceFactory();
+			sourceFactory = null;
+		}
+		// TODO Uncomment when entry point is available
+		//examFactory = Exams.newExamFactory();
+		examFactory = new ExamFactory();
+		
+		answerSource1 = sourceFactory.newSource(testUEFfilename);
+		answerSource1.addText("Foo Bar Answer Text.");
+		
+		answer1 = examFactory.newAnswer(true, answerSource1);
+		
+		answerSource2 = sourceFactory.newSource(testUEFfilename);
+		answerSource2.addText("Bar Foo Answer Text.");
+		
+		answer2 = examFactory.newAnswer(false, answerSource2);
 	}
 	
 	@AfterClass
@@ -40,14 +85,18 @@ public class AnswerTest {
 	}
 
 	@Test
-	@Ignore
 	public void testIsCorrect() {
-		fail("Not yet implemented");
+		assertTrue(answer1.isCorrect());
+		assertFalse(answer2.isCorrect());
 	}
 
 	@Test
-	@Ignore
 	public void testGetSource() {
-		fail("Not yet implemented");
+		SourceIF result = answer1.source();
+		assertNotNull(result);
+		assertEquals(answerSource1, result);
+		assertNotSame(answerSource2, result);
+		assertNotNull(result.text());
+		assertEquals(answerSource1.text(), result.text());
 	}
 }
