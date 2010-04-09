60384c07ad889b3d9c22e2d0e7859ab66dd534a9
boyd <boyd@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-09 20:20:36 +0000

    Initial commit of JUnit tests and stubs for Exam module

---
+++ trunk/test-exam/edu/udel/cis/cisc475/rex/exam/FixedAnswerTest.java
@@ -0,0 +1,46 @@
+/**
+ * rex
+ * edu.udel.cis.cisc475.rex.exam
+ * Apr 9, 2010
+ * hboyd
+ */
+package edu.udel.cis.cisc475.rex.exam;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author hboyd
+ *
+ */
+public class FixedAnswerTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	@Ignore
+	public void testGetIndex() {
+		fail("Not yet implemented");
+	}
+
+}
