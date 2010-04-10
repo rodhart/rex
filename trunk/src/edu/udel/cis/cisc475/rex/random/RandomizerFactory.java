ec870d29bb64dae440bdfe84216d7cd0fb048b35
simons <simons@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-10 21:48:12 +0000

    We added this stub so that our methods would work.

---
+++ trunk/src/edu/udel/cis/cisc475/rex/random/RandomizerFactory.java
@@ -0,0 +1,21 @@
+package edu.udel.cis.cisc475.rex.random;
+
+import edu.udel.cis.cisc475.rex.random.IF.RandomizerFactoryIF;
+import edu.udel.cis.cisc475.rex.random.IF.RandomizerIF;
+
+/**
+ * 
+ * @author team3
+ * We added this stub so that our methods would work.
+ *
+ */
+
+public class RandomizerFactory implements RandomizerFactoryIF {
+
+	@Override
+	public RandomizerIF newRandomizer(long seed) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
