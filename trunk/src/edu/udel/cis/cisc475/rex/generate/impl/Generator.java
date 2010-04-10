792378050d338e1c6761c2e475bd605f3f060fe7
simons <simons@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-10 21:49:00 +0000

    Further defined generate() method.

--- trunk/src/edu/udel/cis/cisc475/rex/generate/impl/Generator.java
+++ trunk/src/edu/udel/cis/cisc475/rex/generate/impl/Generator.java
@@ -1,9 +1,17 @@
 package edu.udel.cis.cisc475.rex.generate.impl;
 
+import java.util.Collection;
+
 import edu.udel.cis.cisc475.rex.config.IF.ConfigIF;
+import edu.udel.cis.cisc475.rex.config.IF.ConstraintIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
+import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorIF;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
+import edu.udel.cis.cisc475.rex.random.RandomizerFactory;
+import edu.udel.cis.cisc475.rex.random.IF.RandomizerFactoryIF;
+import edu.udel.cis.cisc475.rex.random.IF.RandomizerIF;
 
 public class Generator implements GeneratorIF {
 
@@ -22,7 +30,41 @@ public class Generator implements GeneratorIF {
 	}
 
 	private void generate() {
-		// here's where the real work gets done...
+		int numExams = config.numVersions();
+		Collection<ConstraintIF> theConstraints = config.constraints();
+		
+		
+		generatedExams = new ExamIF[numExams];
+		ExamFactoryIF theExamFactory = new ExamFactory();
+		
+		RandomizerFactoryIF theRandomizerFactory = new RandomizerFactory();
+		RandomizerIF theRandomizer = theRandomizerFactory.newRandomizer(config.seed());
+		
+		
+		
+		for (int i = 0; i < numExams; i++)
+		{
+			generatedExams[i] = theExamFactory.newGeneratedExam();
+			
+			for (ConstraintIF c : theConstraints)
+			{
+				/* Test Constraint type; extract information from Constraint.
+				 * Meet constraints using MasterExam (master).
+				 * Pass collection of met constraint problems to randomizer.
+				 * Add what randomizer returns to generatedExams[i].
+				 */
+				
+				if (c.getClass().getName() == "GroupConstraintIF")
+				{
+					//Do stuff if it's a GroupConstraint
+				}
+				
+				if (c.getClass().getName() == "RequiredProblemConstraintIF")
+				{
+					//Do stuff if it's a RequiredProblemConstraint
+				}
+			}	
+		}
 	}
 
 	@Override
