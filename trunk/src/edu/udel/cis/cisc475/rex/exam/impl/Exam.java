b8c6299b7ce58ed3b004babd3b024b694005a862
schultz <schultz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-10 22:40:38 +0000

    First import of problem, figure, front matter, and block methods.

--- trunk/src/edu/udel/cis/cisc475/rex/exam/impl/Exam.java
+++ trunk/src/edu/udel/cis/cisc475/rex/exam/impl/Exam.java
@@ -1,7 +1,10 @@
 package edu.udel.cis.cisc475.rex.exam.impl;
 
 import java.util.Collection;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamElementIF;
@@ -15,7 +18,6 @@ import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
  * 
  * 
  * @author Kevin Schultz (schultz)
- * @author Haley Boyd (boyd)
  * @author Tim McClory (tmcclory)
  *
  */
@@ -23,8 +25,14 @@ public class Exam implements ExamIF {
 	
 	private boolean isMaster;
 
-	private LinkedHashSet elements;
-
+	private LinkedHashMap<Integer, ExamElementIF> elements;
+	
+	private LinkedList<Integer> figures;
+	
+	private LinkedList<Integer> blocks;
+	
+	private LinkedList<Integer> problems;
+	
 	private SourceIF preamble; 
 	
 	private SourceIF frontmatter;
@@ -38,55 +46,73 @@ public class Exam implements ExamIF {
 	public Exam(boolean isMaster) {
 		super();
 		this.isMaster = isMaster;
+		this.elements = new LinkedHashMap<Integer, ExamElementIF>();
+		this.figures = new LinkedList<Integer>();
+		this.blocks = new LinkedList<Integer>();
+		this.problems = new LinkedList<Integer>();
+		
 	}
 
 
-	//@Override
 	public void addElementIF(ExamElementIF element) {
-		// TODO Auto-generated method stub
+		int key = elements.size() + 1;
+		//put into linked hash set
+		elements.put(key,element);
+		//also need to make some kind of record of whether it is
+		//a problem, a block, or a figure. 
+		if (element.getClass().isInstance(FigureIF.class)) {
+			figures.add(key);
+		} else if (element.getClass().isInstance(BlockIF.class)) {
+			blocks.add(key);
+		} else if (element.getClass().isInstance(ProblemIF.class)) {
+			problems.add(key);
+		} else {
+			//Do we need to do anything in this case? It definitely is not good if it isn't one of the 3
+		}
+		
+		
 
 	}
 
-	//@Override
 	public void declareUse(ExamElementIF user, ExamElementIF usee) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-	//@Override
 	public ExamElementIF element(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public ExamElementIF elementWithLabel(String label) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public Collection<ExamElementIF> elements() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public Collection<ExamElementIF> elementsUsingElement(ExamElementIF element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public Collection<ExamElementIF> elementsWithTopic(String topic) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public Collection<FigureIF> figures() {
-		// TODO Auto-generated method stub
-		return null;
+		HashSet<FigureIF> returnSet = new HashSet<FigureIF>();
+		
+		Iterator<Integer> i = figures.iterator();
+		while(i.hasNext()) {
+			returnSet.add((FigureIF) elements.get(i.next()));
+		}
+		
+		return (Collection<FigureIF>) returnSet;
 	}
 
 	public BlockIF finalBlock() {
@@ -101,13 +127,11 @@ public class Exam implements ExamIF {
 		return this.isMaster;
 	}
 
-	//@Override
 	public Collection<String> labels() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//@Override
 	public int numElements() {
 		// TODO Auto-generated method stub
 		return 0;
@@ -117,16 +141,34 @@ public class Exam implements ExamIF {
 		return this.preamble;
 	}
 
-	//@Override
+	/**
+	 * 
+	 */
 	public Collection<ProblemIF> problems() {
-		// TODO Auto-generated method stub
-		return null;
+		HashSet<ProblemIF> returnSet = new HashSet<ProblemIF>();
+		
+		Iterator<Integer> i = problems.iterator();
+		while(i.hasNext()) {
+			returnSet.add((ProblemIF) elements.get(i.next()));
+		}
+		
+		return (Collection<ProblemIF>) returnSet;
 	}
 
-	//@Override
+	
 	public Collection<ProblemIF> problemsWithTopic(String topic) {
-		// TODO Auto-generated method stub
-		return null;
+		HashSet<ProblemIF> returnSet = new HashSet<ProblemIF>();
+		ProblemIF problem;
+		
+		Iterator<Integer> i = problems.iterator();
+		while(i.hasNext()) {
+			problem = (ProblemIF) elements.get(i.next());
+			if(problem.topic().equals(topic)) {
+				returnSet.add(problem);
+			}
+		}
+		
+		return (Collection<ProblemIF>) returnSet;
 	}
 
 	public void setFinalBlock(BlockIF block) {
