c8221000a1dfbce558c016bd8c16484d5d2cbabf
simons <simons@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-10 21:46:52 +0000

    GroupConstraintIF and RequiredProblemConstraintIF were changed to extend ConstraintIF.

--- src/edu/udel/cis/cisc475/rex/config/IF/GroupConstraintIF.java
+++ src/edu/udel/cis/cisc475/rex/config/IF/GroupConstraintIF.java
@@ -4,10 +4,11 @@ import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
 
 /*
  * @author team 2
- *
+ * Edited by team 3 to correctly reflect the design document (5pm, 4/10/2010)
+ * (Extended ConstraintIF)
  */
 
-public interface GroupConstraintIF {
+public interface GroupConstraintIF extends ConstraintIF{
 	int numProblems();
 
 	IntervalIF difficultyInterval();
--- src/edu/udel/cis/cisc475/rex/config/IF/RequiredProblemConstraintIF.java
+++ src/edu/udel/cis/cisc475/rex/config/IF/RequiredProblemConstraintIF.java
@@ -2,10 +2,11 @@ package edu.udel.cis.cisc475.rex.config.IF;
 
 /*
  * @author team 2
- *
+ * Edited by team 3 to correctly reflect the design document (5:30pm, 4/10/2010)
+ * (Extended ConstraintIF)
  */
 
-public interface RequiredProblemConstraintIF {
+public interface RequiredProblemConstraintIF extends ConstraintIF{
 	String label();
 
 	int points();
