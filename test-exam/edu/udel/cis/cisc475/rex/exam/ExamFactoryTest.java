53097927d4e7b0724479adf82d977f4d97075cdc
boyd <boyd@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-10 23:31:47 +0000

    Initial implementation of ExamFactoryTest methods

--- test-exam/edu/udel/cis/cisc475/rex/exam/ExamFactoryTest.java
+++ test-exam/edu/udel/cis/cisc475/rex/exam/ExamFactoryTest.java
@@ -6,14 +6,29 @@
  */
 package edu.udel.cis.cisc475.rex.exam;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamElementIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FixedAnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
+import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.examstubs.SourceFactoryStub;
 
 /**
  * @author hboyd
@@ -22,14 +37,24 @@ import org.junit.Test;
 public class ExamFactoryTest {
 	public final static boolean useStubs = true;
 	
+	private static SourceFactoryIF sourceFactory;
+	private static ExamFactoryIF examFactory;
+	
+	private static String testUEFfilename = "testFileName.txt";
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		if (useStubs) {
-			
+			sourceFactory = new SourceFactoryStub();
 		}
 		else{
-			
+			// TODO Uncomment when entry point is available
+			//sourceFactory = Sources.newSourceFactory();
+			sourceFactory = null;
 		}
+		// TODO Uncomment when entry point is available
+		//examFactory = Exams.newExamFactory();
+		examFactory = new ExamFactory();
 	}
 	
 	@AfterClass
@@ -45,44 +70,76 @@ public class ExamFactoryTest {
 	}
 
 	@Test
-	@Ignore
 	public void testNewMasterExam() {
-		fail("Not yet implemented");
+		ExamIF exam = examFactory.newMasterExam();
+		assertNotNull(exam);
+		assertTrue(exam.isMaster());
 	}
 
 	@Test
-	@Ignore
 	public void testNewGeneratedExam() {
-		fail("Not yet implemented");
+		ExamIF exam = examFactory.newGeneratedExam();
+		assertNotNull(exam);
+		assertFalse(exam.isMaster());
 	}
 	
 	@Test
-	@Ignore
 	public void testNewBlock() {
-		fail("Not yet implemented");
+		SourceIF blockSource = sourceFactory.newSource(testUEFfilename);
+		blockSource.addText("test Block Source");
+		ExamElementIF block = examFactory.newBlock("test Topic", "test Label", blockSource);
+		assertNotNull(block);
+		assertTrue(block instanceof BlockIF);
 	}
 	
 	@Test
-	@Ignore
 	public void testNewFigure() {
-		fail("Not yet implemented");
+		SourceIF figureSource = sourceFactory.newSource(testUEFfilename);
+		figureSource.addText("test Figure Source");
+		ExamElementIF figure = examFactory.newFigure("test figure label", figureSource);
+		assertNotNull(figure);
+		assertTrue(figure instanceof FigureIF);
 	}
 	
 	@Test
-	@Ignore
 	public void testNewAnswer() {
-		fail("Not yet implemented");
+		SourceIF answerSource = sourceFactory.newSource(testUEFfilename);
+		answerSource.addText("test Answer Source");
+		AnswerIF answer1 = examFactory.newAnswer(true, answerSource);
+		assertNotNull(answer1);
+		AnswerIF answer2 = examFactory.newAnswer(false, answerSource);
+		assertNotNull(answer2);
+		assertNotSame(answer1, answer2);
 	}
 	
 	@Test
-	@Ignore
 	public void testNewFixedAnswer() {
-		fail("Not yet implemented");
+		SourceIF answerSource = sourceFactory.newSource(testUEFfilename);
+		answerSource.addText("test Answer Source");
+		AnswerIF answer1 = examFactory.newFixedAnswer(true, 0, answerSource);
+		assertNotNull(answer1);
+		assertTrue(answer1 instanceof FixedAnswerIF);
+		AnswerIF answer2 = examFactory.newFixedAnswer(false, 0, answerSource);
+		assertNotNull(answer2);
+		assertTrue(answer1 instanceof FixedAnswerIF);
+		assertNotSame(answer1, answer2);
 	}
 	
 	@Test
-	@Ignore
 	public void testNewProblem() {
-		fail("Not yet implemented");
+		SourceIF answerSource = sourceFactory.newSource(testUEFfilename);
+		answerSource.addText("test Answer Source");
+		AnswerIF answer1 = examFactory.newAnswer(true, answerSource);
+		AnswerIF answer2 = examFactory.newFixedAnswer(false, 0, answerSource);
+		AnswerIF[] answers = new AnswerIF[2];
+		answers[0] = answer1;
+		answers[1] = answer2;
+		
+		SourceIF problemSource = sourceFactory.newSource(testUEFfilename);
+		problemSource.addText("test Problem Source?");
+		
+		ExamElementIF problem = examFactory.newProblem("test Problem Topic", "test Label", problemSource, answers);
+		assertNotNull(problem);
+		assertTrue(problem instanceof ProblemIF);
 	}
 }
