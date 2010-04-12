5daeb39674ba1ad599bab85d9fa38be4a9a32244
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 22:00:40 +0000

    added junit test file for random

---
+++ test-random/edu/udel/cis/cisc475/rex/random/random_junit_tester.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.udel.cis.cisc475.rex.random;
+
+/**
+ * @author cardona
+ *
+ */
+public class random_junit_tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}//end of class
+
+
