d57ad71fc1f37a7e4979c5f689d8264fd9afe0a2
zhine <zhine@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 23:48:54 +0000

    Added a stub for IntervalFactory.java. We needed to create a new IntervalFactory for the generate() method, so this ensures it will compile.

---
+++ trunk/src/edu/udel/cis/cisc475/rex/interval/impl/IntervalFactory.java
@@ -0,0 +1,22 @@
+package edu.udel.cis.cisc475.rex.interval.impl;
+
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalFactoryIF;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
+
+public class IntervalFactory implements IntervalFactoryIF
+{
+	
+	/**
+	 * Added by Team 3 
+	 * 7:47 4/12/10
+	 * Needed a stub for Generator's generate() method
+	 */
+
+	@Override
+	public IntervalIF interval(boolean strictLow, Double low,
+			boolean strictHigh, Double high) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
