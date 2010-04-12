2ebbf7be835198e829a30da86071384e697b208a
jsong <jsong@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 21:52:32 +0000

    Working filename for testing

--- src/edu/udel/cis/cisc475/rex/source/source_test.java
+++ src/edu/udel/cis/cisc475/rex/source/source_test.java
@@ -3,7 +3,8 @@ package edu.udel.cis.cisc475.rex.source;
 import java.io.PrintWriter;
 
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
-import edu.udel.cis.cisc475.rex.source.impl.Source;
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
 
 /**
  * @author team 5
@@ -16,34 +17,30 @@ public class source_test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String filename = "DrHarvey_source_test.tex";
-
+		String filename = "./trunk/src/edu/udel/cis/cisc475/rex/source/DrHarvey_source_test.tex";
 		
-//		newSource(filename);
+		// Creates a new SourceFactory
+		SourceFactoryIF ourFactory = new SourceFactory();
+		// Gets a new instance of Source
+		SourceIF S = ourFactory.newSource(filename);
 		
 		//declare a pointer to an interface and
 		//create an object of the interface type
-		SourceIF S = new Source(filename);
-		
-		
 		
 	 	System.out.printf("filename is %s \n", S.filename());			
 	 	System.out.printf("read in file\n");			
 
 	 	System.out.printf("now display what is in our array \n");
 		PrintWriter pw = new PrintWriter(System.out,true); 
-//		S.write(pw); 
+		S.write(pw); 
 
-	 	
-	 	
-	 	System.out.printf("now add some text to our array \n\n");
-		S.addText("this is test line 1");
-		S.addText("      this is the second test line");
+//	 	System.out.printf("now add some text to our array \n\n");
+//		S.addText("this is test line 1");
+//		S.addText("      this is the second test line");
 
 		System.out.printf("now display what is in our array \n");
 		S.write(pw); 
 
-		
 		System.out.printf("startline is %d \n", S.startLine());			
 		System.out.printf("lastLine is %d \n", S.lastLine());
 		
@@ -51,7 +48,7 @@ public class source_test {
 		S.setStartLine(0);			
 		System.out.printf("startline is %d \n", S.startLine());			
 		System.out.printf("setting lastline to 3\n");
-		S.setLastLine(1);			
+		S.setLastLine(3);			
 		System.out.printf("lastline is %d \n", S.lastLine());			
 		System.out.printf("now display between bounds lines 0 to 3 - will show lines 0, 1, 2\n");
 		S.write(pw); 
