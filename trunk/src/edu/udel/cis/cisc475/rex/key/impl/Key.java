6649a08c8590cfc5d6f4d04054d4a5c1a428196a
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 20:59:42 +0000

    fixed key test main to properly call key, also added bounds check to key

--- trunk/src/edu/udel/cis/cisc475/rex/key/impl/Key.java
+++ trunk/src/edu/udel/cis/cisc475/rex/key/impl/Key.java
@@ -19,6 +19,8 @@ public Key(String version, String examName, String date) {
 	_examName = examName;	
 	_date = date;	
 	_Answers = new ArrayList< Collection<String>  >();
+	error_out_of_bounds = new ArrayList<String>();
+	error_out_of_bounds.add(new String("error_out_of_bounds"));
 }//end of constructor
 
 //getters
@@ -26,7 +28,14 @@ public	String 	version () 	{return _version;}
 public  String 	examName()	{return _examName;} 
 public  String 	date()		{return _date;} 
 public  int 	numProblems() {return _numAnswers;} 
-public Collection <String> answers(int index) {	return _Answers.get(index);} 
+
+public Collection <String> answers(int index) {	
+	//since _numAnswers is 1 based and index is 0 based use less than
+	if (index<_numAnswers)
+	return _Answers.get(index);
+		//else
+ 		return error_out_of_bounds;
+}
 
 //setters
 public void addProblem( Collection <String> nextAnswer) {
@@ -40,4 +49,5 @@ private String _examName;
 private String _date;	
 private int _numAnswers;
 private ArrayList < Collection<String> > _Answers;
+private  Collection <String> error_out_of_bounds;
 }//end of class 
