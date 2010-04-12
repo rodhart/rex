892672dd80f302c4df173b3c6c7ada1369aea8e6
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 21:41:55 +0000

    wrote answer key javadocs

--- src/edu/udel/cis/cisc475/rex/key/Keys.java
+++ src/edu/udel/cis/cisc475/rex/key/Keys.java
@@ -3,10 +3,27 @@ package edu.udel.cis.cisc475.rex.key;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyFactoryIF;
 import edu.udel.cis.cisc475.rex.key.impl.AnswerKeyFactory;
 
+/**	
+* this class uses a static method used to create the factory which creates 
+* a new instance of an answer key
+* 
+* this is an alternate way to do factories with interfaces
+* instead of creating an instance of the factory (saves memory)
+*   @author siegel / javadoc by cardona
+* 
+*/
 public class Keys {
 
+	/**	
+	* static method used to create the factory which creates 
+	* a new instance of an answer key
+	* 
+	* this is an alternate way to do factories with interfaces
+	* instead of creating an instance of the factory (saves memory)
+	* 
+	*/
 	public static AnswerKeyFactoryIF newAnswerKeyFactory() {
 		return new AnswerKeyFactory();
 	}
 
-}
+}//end of class
