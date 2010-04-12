ba621bbc1cae2ef53a302a9eac7c1946264838a4
martz <martz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 19:44:46 +0000

    ExamFactory.java: Added JavaDoc comments to methods.

--- trunk/src/edu/udel/cis/cisc475/rex/exam/impl/ExamFactory.java
+++ trunk/src/edu/udel/cis/cisc475/rex/exam/impl/ExamFactory.java
@@ -19,38 +19,66 @@ import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
  */
 public class ExamFactory implements ExamFactoryIF {
 
-	// @Override
+	/**
+	 * Creates a new instance of AnswerIF.
+	 * 
+	 * @return AnswerIF
+	 */
 	public AnswerIF newAnswer(boolean correct, SourceIF text) {
 		return new Answer(correct, text);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance of BlockIF.
+	 * 
+	 * @return BlockIF
+	 */
 	public BlockIF newBlock(String topic, String label, SourceIF text) {
 		return new Block(topic, label, text);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance of FigureIF.
+	 * 
+	 * @return FigureIF
+	 */
 	public FigureIF newFigure(String label, SourceIF text) {
 		return new Figure(label, text);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance of FixedAnswerIF.
+	 * 
+	 * @return FixedAnswerIF
+	 */
 	public FixedAnswerIF newFixedAnswer(boolean correct, int index,
 			SourceIF text) {
 		return new FixedAnswer(index, correct, text);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance of ExamIF for use as a output Exam.
+	 * 
+	 * @return ExamIF
+	 */
 	public ExamIF newGeneratedExam() {
 		return new Exam(false);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance ExamIF for use as the master repository.
+	 * 
+	 * @return ExamIF
+	 */
 	public ExamIF newMasterExam() {
 		return new Exam(true);
 	}
 
-	// @Override
+	/**
+	 * Creates a new instance of ProblemIF.
+	 * 
+	 * @return ProblemIF
+	 */
 	public ProblemIF newProblem(String topic, String label, SourceIF question, AnswerIF[] answers) {
 		return new Problem(topic, label, question, answers);
 	}
