315aa203da8bd3905760f1c58edf9432360ff372
alandweh <alandweh@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 17:08:01 +0000

    In the UEFParser: Unified handling of command and environments together instead of separately. Modified reading from the file to give an error instead of silently failing at the end of the file. Modified the peakUntil() method to only consider our special commands when peaking ahead.

--- trunk/src/edu/udel/cis/cisc475/rex/uefparser/UEFParser.java
+++ trunk/src/edu/udel/cis/cisc475/rex/uefparser/UEFParser.java
@@ -11,19 +11,17 @@ import java.io.InputStreamReader;
 
 /**
  * Parser for the Universal Exam File (UEF).
- * 
+ *
  * FIXME: Doesn't handle /labels{}.
- * 
+ *
  * FIXME: Does not handle optional arguments in the form of /command[].
- * 
- * FIXME: Doesn't handle the escape character '\\'.
- * 
+ *
  * FIXME: Doesn't handle \begin{verbatim} \end{verbatim} environment.
- * 
+ *
  * FIXME: Doesn't handle the \verb command.
- * 
+ *
  * @author Aaron Myles Landwehr
- * 
+ *
  */
 public class UEFParser implements UEFParserIF
 {
@@ -36,23 +34,16 @@ public class UEFParser implements UEFParserIF
 	private Command command = new Command();
 
 	// The latex commands that we currently understand.
-	//Be careful to not add any commands that we don't want to be peaked ahead.
-	//Otherwise Command.peakUntil() will break.
-	//FIXME: Command.peakUntil() should have a list of commands it treats specially.
-	private enum CommandTypes {
+	private enum CommandTypes
+	{
 
-		documentclass, begin, end, answer, none
-	}
-
-	// The latex environments that we currently understand.
-	private enum EnvironmentTypes {
-
-		problem, answers, none
+		documentclass, beginVerbatim, endVerbatim, beginProblem, endProblem, beginAnswers,
+		endAnswers, answer, none
 	}
 
 	/**
 	 * Returns whether our current position in the stream is within a comment.
-	 * 
+	 *
 	 * @return true if we are in a comment and false if we are not.
 	 */
 	private boolean isComment()
@@ -74,11 +65,11 @@ public class UEFParser implements UEFParserIF
 		/**
 		 * Opens a file and completely reads it into a StringBuffer for easy
 		 * parsing.
-		 * 
+		 *
 		 * @param file
 		 *            The file to read from.
 		 * @throws IOException
-		 * 
+		 *
 		 */
 		private void openFile(File file) throws IOException
 		{
@@ -86,8 +77,7 @@ public class UEFParser implements UEFParserIF
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					stream));
 
-			for (String line = reader.readLine(); line != null; line = reader
-					.readLine())
+			for (String line = reader.readLine(); line != null; line = reader.readLine())
 			{
 				fileContents.append(line);
 				fileContents.append('\n');
@@ -105,18 +95,27 @@ public class UEFParser implements UEFParserIF
 
 		/**
 		 * Reads one character from the underlying file and returns it.
-		 * 
+		 *
 		 * @return The character read.
-		 * 
+		 *
 		 */
 		private char read()
 		{
-			return fileContents.charAt(position);
+			try
+			{
+				return fileContents.charAt(position);
+			}
+			catch (IndexOutOfBoundsException e)
+			{
+				System.out.println("FIXME: I'm not handled when the end of file is reached...");
+				System.exit(-1);
+			}
+			return 0;
 		}
 
 		/**
 		 * Returns a range of characters from the underlying file.
-		 * 
+		 *
 		 * @param start
 		 *            The beginning index inclusive.
 		 * @param end
@@ -130,7 +129,7 @@ public class UEFParser implements UEFParserIF
 
 		/**
 		 * Returns the current position in the file.
-		 * 
+		 *
 		 * @return The current position in the file.
 		 */
 		private int getPosition()
@@ -140,7 +139,7 @@ public class UEFParser implements UEFParserIF
 
 		/**
 		 * Sets the current position in the file.
-		 * 
+		 *
 		 * @param position
 		 *            The position to set the file to.
 		 */
@@ -156,84 +155,13 @@ public class UEFParser implements UEFParserIF
 	private class Command
 	{
 
-		// Instance of an environment class to handle things related to latex
-		// environments
-		private Environment environment = new Environment();
-
-		/**
-		 * This subclass handles everything related to environments.
-		 */
-		private class Environment
-		{
-
-			/**
-			 * Inteprets the environment we are at from the current string.
-			 * 
-			 * E.g. if the String is "problem" then returns
-			 * EvironmentTypes.problem
-			 * 
-			 * @param environment
-			 *            A string representing a possible environment.
-			 * @return The environment type found if any. If no environment is
-			 *         found then returns EnvironmentTypes.none.
-			 */
-			private EnvironmentTypes process(String environment)
-			{
-				if (environment.equals("problem"))
-				{
-					return EnvironmentTypes.problem;
-				}
-				else if (environment.equals("answers"))
-				{
-					return EnvironmentTypes.answers;
-				}
-				else
-				{
-					return EnvironmentTypes.none;
-				}
-			}
-
-			/**
-			 * Handles the \begin{problem} environment.
-			 * 
-			 */
-			private void processProblem()
-			{
-				// get the arguments for Problem
-				String[] arguments = getArguments(2);
-
-				String topic = arguments[0];// grab the topic
-				String difficulty = arguments[1];// grab the difficulty
-
-				// do some stuff we don't need to do
-				System.out.println("Found Problem Environment with topic '"
-						+ topic + "' of difficulty '" + difficulty
-						+ "' with the following text:");
-
-				// get text for the problem
-				String buffer = peakUntil();
-				System.out.println("-----------------------------------");
-				System.out.println(buffer);
-				System.out.println("-----------------------------------");
-				System.out.println();
-			}
-
-			/**
-			 * Handles the \begin{answers} environment.
-			 * 
-			 */
-			private void processAnswers()
-			{
-				// FIXME: we need to process this.
-				System.out.println("Found Answers Environment");
-			}
-		}
+		int lastPosition = -1;
 
 		/**
 		 * Starting at the current position in the file this reads until the
 		 * next one of our handled commands and returns a String with all the
 		 * characters read.
-		 * 
+		 *
 		 * @return A String with all characters read
 		 */
 		private String peakUntil()
@@ -245,7 +173,10 @@ public class UEFParser implements UEFParserIF
 			CommandTypes commandType = command.process();
 
 			// loop until we find one of our commands
-			while (commandType.equals(CommandTypes.none))
+			while (!commandType.equals(CommandTypes.beginAnswers) && !commandType.equals(
+					CommandTypes.endAnswers) && !commandType.equals(CommandTypes.beginProblem)
+				   && !commandType.equals(CommandTypes.endProblem) && !commandType.equals(
+					CommandTypes.answer))
 			{
 				// process until we find a command
 				commandType = command.process();
@@ -253,9 +184,7 @@ public class UEFParser implements UEFParserIF
 
 			// grap all the characters inbetween our original position and our
 			// new position
-			String peakedData = uef.getContent(currentPosition, uef
-					.getPosition() -
-					commandType.toString().length() - 2);
+			String peakedData = uef.getContent(currentPosition, command.lastPosition);
 
 			// reset our position
 			uef.setPosition(currentPosition);
@@ -267,7 +196,7 @@ public class UEFParser implements UEFParserIF
 		/**
 		 * Reads until a command and then gets the command and returns it as a
 		 * String. Always moves passed the commands characters in the file.
-		 * 
+		 *
 		 * @return the current command as a string without the '\' prefix
 		 */
 		private CommandTypes process()
@@ -289,18 +218,18 @@ public class UEFParser implements UEFParserIF
 				// whether it is in a comment or not.
 				if (uef.read() == '\\' && !isComment())
 				{
-
+					lastPosition = uef.getPosition();
 					String command = new String();
 
 					// move forward in the file.
 					uef.move();
 
 					//check to see if the command is an escape character
-					if(uef.read()=='\\')
+					if (uef.read() == '\\')
 					{
-					    //move forward because we don't care about this command
-					    uef.move();
-					    return CommandTypes.none;
+						//move forward because we don't care about this command
+						uef.move();
+						return CommandTypes.none;
 					}
 
 					// Grab each letter in the command while there are letters
@@ -308,36 +237,60 @@ public class UEFParser implements UEFParserIF
 					// remember only letters are valid for command names
 					while (Character.isLetter(uef.read()))
 					{
-					    command = command + uef.read();
-					    uef.move();
+						command = command + uef.read();
+						uef.move();
 					}
 
 					// Below just returns the type of command we found.
 					if (command.equals("documentclass"))
 					{
-					    uef.move();
-					    return CommandTypes.documentclass;
+						uef.move();
+						return CommandTypes.documentclass;
 					}
 					else if (command.equals("begin"))
 					{
-					    uef.move();
-					    return CommandTypes.begin;
+						String arguments[] = getArguments(1);
+						String environment = arguments[0];
+						if (environment.equals("problem"))
+						{
+							return CommandTypes.beginProblem;
+						}
+						else if (environment.equals("answers"))
+						{
+							return CommandTypes.beginAnswers;
+						}
+						else
+						{
+							return CommandTypes.none;
+						}
 					}
 					else if (command.equals("end"))
 					{
-					    uef.move();
-					    return CommandTypes.end;
+						String arguments[] = getArguments(1);
+						String environment = arguments[0];
+						if (environment.equals("problem"))
+						{
+							return CommandTypes.endProblem;
+						}
+						else if (environment.equals("answers"))
+						{
+							return CommandTypes.endAnswers;
+						}
+						else
+						{
+							return CommandTypes.none;
+						}
 					}
 					else if (command.equals("answer"))
 					{
-					    uef.move();
-					    return CommandTypes.answer;
+						uef.move();
+						return CommandTypes.answer;
 					}
 					else
 					{
-					    // A command we don't handle or care about.
-					    uef.move();
-					    return CommandTypes.none;
+						// A command we don't handle or care about.
+						uef.move();
+						return CommandTypes.none;
 					}
 				}
 			}
@@ -345,7 +298,7 @@ public class UEFParser implements UEFParserIF
 
 		/**
 		 * Handles the \documentclass command.
-		 * 
+		 *
 		 */
 		private void processDocumentclass()
 		{
@@ -359,50 +312,8 @@ public class UEFParser implements UEFParserIF
 		}
 
 		/**
-		 * Handles the \begin command.
-		 * 
-		 */
-		private void processBegin()
-		{
-			// get the type of environment
-			String[] arguments = getArguments(1);
-			EnvironmentTypes environmentType = environment
-					.process(arguments[0]);
-			// check for environments we handle
-			if (environmentType.equals(EnvironmentTypes.problem))
-			{
-				environment.processProblem();
-			}
-			else if (environmentType.equals(EnvironmentTypes.answers))
-			{
-				environment.processAnswers();
-			}
-		}
-
-		/**
-		 * Handles the \end command.
-		 * 
-		 */
-		private void processEnd() throws IOException
-		{
-			// get the type of environment
-			String[] arguments = getArguments(1);
-			EnvironmentTypes environmentType = environment
-					.process(arguments[0]);
-
-			if (environmentType.equals(EnvironmentTypes.problem))
-			{
-				// do something maybe
-			}
-			else if (environmentType.equals(EnvironmentTypes.answers))
-			{
-				// do something maybe
-			}
-		}
-
-		/**
 		 * Handles the Answer command.
-		 * 
+		 *
 		 */
 		private void processAnswer()
 		{
@@ -413,6 +324,73 @@ public class UEFParser implements UEFParserIF
 			System.out.println(buffer);
 			System.out.println("-----------------------------------");
 			System.out.println();
+		}
+
+		/**
+		 * Handles the \begin{verbatim} environment.
+		 *
+		 */
+		private void processBeginVerbatim()
+		{
+		}
+
+		/**
+		 * Handles the \end{verbatim} environment.
+		 *
+		 */
+		private void processEndVerbatim()
+		{
+		}
+
+		/**
+		 * Handles the \begin{problem} environment.
+		 *
+		 */
+		private void processBeginProblem()
+		{
+			// get the arguments for Problem
+			String[] arguments = getArguments(2);
+
+			String topic = arguments[0];// grab the topic
+			String difficulty = arguments[1];// grab the difficulty
+
+			// do some stuff we don't need to do
+			System.out.println("Found Problem Environment with topic '"
+							   + topic + "' of difficulty '" + difficulty
+							   + "' with the following text:");
+
+			// get text for the problem
+			String buffer = peakUntil();
+			System.out.println("-----------------------------------");
+			System.out.println(buffer);
+			System.out.println("-----------------------------------");
+			System.out.println();
+		}
+
+		/**
+		 * Handles the \end{problem} environment.
+		 *
+		 */
+		private void processEndProblem()
+		{
+		}
+
+		/**
+		 * Handles the \begin{answers} environment.
+		 *
+		 */
+		private void processBeginAnswers()
+		{
+			// FIXME: we need to process this.
+			System.out.println("Found Answers Environment");
+		}
+
+		/**
+		 * Handles the \end{answers} environment.
+		 *
+		 */
+		private void processEndAnswers()
+		{
 		}
 	}
 
@@ -437,13 +415,13 @@ public class UEFParser implements UEFParserIF
 	 * Starting at the currently retrieved character from the stream this method
 	 * returns arguments surrounded by squiggle brackets. Yes, misspelled
 	 * squiggle brackets.
-	 * 
+	 *
 	 * FIXME: Needs error checking for when there are any character other than
 	 * spaces and new lines between arguments.
-	 * 
+	 *
 	 * FIXME: Needs to check for comments and ignore arguments found within
 	 * those.
-	 * 
+	 *
 	 * @param numberOfArguments
 	 *            The number of arguments you want to retrieve
 	 * @return A string array with the arguments as Strings
@@ -480,7 +458,7 @@ public class UEFParser implements UEFParserIF
 
 	/**
 	 * Parse UEF file and generate ExamIF from it.
-	 * 
+	 *
 	 * @param file
 	 *            the file handler for the uef file.
 	 * @return ExamIF of the uef file.
@@ -500,18 +478,35 @@ public class UEFParser implements UEFParserIF
 				{
 					command.processDocumentclass();
 				}
-				else if (commandType.equals(CommandTypes.begin))
-				{
-					command.processBegin();
-				}
-				else if (commandType.equals(CommandTypes.end))
-				{
-					command.processEnd();
-				}
 				else if (commandType.equals(CommandTypes.answer))
 				{
 					command.processAnswer();
 				}
+				else if (commandType.equals(CommandTypes.beginVerbatim))
+				{
+					command.processBeginVerbatim();
+				}
+				else if (commandType.equals(CommandTypes.endVerbatim))
+				{
+					command.processBeginVerbatim();
+				}
+				else if (commandType.equals(CommandTypes.beginProblem))
+				{
+					command.processBeginProblem();
+				}
+				else if (commandType.equals(CommandTypes.endProblem))
+				{
+					command.processEndProblem();
+				}
+				else if (commandType.equals(CommandTypes.beginAnswers))
+				{
+					command.processBeginAnswers();
+				}
+				else if (commandType.equals(CommandTypes.endAnswers))
+				{
+					command.processEndAnswers();
+				}
+
 			}
 		}
 		catch (FileNotFoundException e)
@@ -521,7 +516,8 @@ public class UEFParser implements UEFParserIF
 		}
 		catch (StringIndexOutOfBoundsException e)
 		{
-			System.out.println("End of file reached.");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		catch (Exception e)
 		{
