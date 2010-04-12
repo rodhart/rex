c7f0dd4f1701c3555a901b64a6768b85c5e1fa88
zhine <zhine@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-12 05:16:08 +0000

    Further defined the generate() method. Edited the if statements to account for the case of additional interfaces that could extend ConstraintIF. Added method calls to allow us to store the data in the fields required by a Constraint. Then, added comments to explain what remains to be done.

--- trunk/src/edu/udel/cis/cisc475/rex/generate/impl/Generator.java
+++ trunk/src/edu/udel/cis/cisc475/rex/generate/impl/Generator.java
@@ -4,23 +4,25 @@ import java.util.Collection;
 
 import edu.udel.cis.cisc475.rex.config.IF.ConfigIF;
 import edu.udel.cis.cisc475.rex.config.IF.ConstraintIF;
+import edu.udel.cis.cisc475.rex.config.IF.GroupConstraintIF;
+import edu.udel.cis.cisc475.rex.config.IF.RequiredProblemConstraintIF;
+import edu.udel.cis.cisc475.rex.err.RexUnsatisfiableException;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorIF;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
 import edu.udel.cis.cisc475.rex.random.RandomizerFactory;
 import edu.udel.cis.cisc475.rex.random.IF.RandomizerFactoryIF;
 import edu.udel.cis.cisc475.rex.random.IF.RandomizerIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
 
 public class Generator implements GeneratorIF {
 
 	private ExamIF master;
-
 	private ConfigIF config;
-
 	private AnswerKeyIF[] keys;
-
 	private ExamIF[] generatedExams;
 
 	Generator(ExamIF master, ConfigIF config) {
@@ -29,40 +31,88 @@ public class Generator implements GeneratorIF {
 		generate();
 	}
 
-	private void generate() {
+	private void generate() 
+	{
 		int numExams = config.numVersions();
 		Collection<ConstraintIF> theConstraints = config.constraints();
-		
-		
+			
 		generatedExams = new ExamIF[numExams];
 		ExamFactoryIF theExamFactory = new ExamFactory();
 		
 		RandomizerFactoryIF theRandomizerFactory = new RandomizerFactory();
 		RandomizerIF theRandomizer = theRandomizerFactory.newRandomizer(config.seed());
 		
-		
-		
+
 		for (int i = 0; i < numExams; i++)
 		{
 			generatedExams[i] = theExamFactory.newGeneratedExam();
 			
 			for (ConstraintIF c : theConstraints)
 			{
-				/* Test Constraint type; extract information from Constraint.
+				/* Overview:
+				 * Test Constraint type; extract information from Constraint.
 				 * Meet constraints using MasterExam (master).
 				 * Pass collection of met constraint problems to randomizer.
 				 * Add what randomizer returns to generatedExams[i].
-				 */
+				 */				
 				
-				if (c.getClass().getName() == "GroupConstraintIF")
+				if (c.getClass().isInstance(GroupConstraintIF.class))	
 				{
-					//Do stuff if it's a GroupConstraint
+					// cast to a GroupConstraintIF
+					GroupConstraintIF gc = (GroupConstraintIF) c;
+
+					// extract relevant data
+					SourceIF source = gc.source(); // necessary?
+					int numProblems = gc.numProblems();
+					IntervalIF difficultyInterval = gc.difficultyInterval();
+					String topic = gc.topic();
+					int points = gc.points();
+				
+					// loop until 'master' END OF FILE
+					// we can probably define an Iterator i and do a while(i.hasNext())
+					// fill a container with all problems that match the difficulty / topic / points constraints
+					// (will be of type ExamElementIF)
+					
+					// then make a call the randomizer's choose() method
+					// like, theRandomizer.choose(numProblems, Collection<ExamElementIF> theProblems)
+					
+					// the choose() method returns a randomized array of Objects[] equal to numProblems
+					
+					// now, do any necessary type casting
+					
+					// now call the ExamFactory addProblem method and add it to generatedExams[i]
+					
 				}
 				
-				if (c.getClass().getName() == "RequiredProblemConstraintIF")
+				else if (c.getClass().isInstance(RequiredProblemConstraintIF.class))	
 				{
-					//Do stuff if it's a RequiredProblemConstraint
+					// cast to a RequiredProblemConstraintIF
+					RequiredProblemConstraintIF rpc = (RequiredProblemConstraintIF) c;
+				
+					// extract relevant data
+					SourceIF source = rpc.source(); // necessary?
+					String label = rpc.label();
+					int points = rpc.points();
+					
+					// repeat everything listed above
+					
+					// Question: A RequiredProblemConstraint object contains a label for whatever the
+					// required Figure / Block is, and a possible points value. But if we're iterating
+					// through a myriad of GroupConstraints AND RequiredProblemConstraints, and the
+					// RequiredProblemConstraints are the only things telling us when we need a figure,
+					// I'm assuming that order matters in the config file, right? 
+					// What I mean is: unless a RequiredProblemConstraint for a figure is listed in 
+					// the config file directly before the GroupConstraints outlining the questions
+					// it references, how else will we know to group the two together? The only thing
+					// logically linking the two constraints is that they both extend the ConstraintIF
 				}
+				
+				else
+				{
+					// throw new RexUnsatisfiableException();
+				}
+				
+				
 			}	
 		}
 	}
