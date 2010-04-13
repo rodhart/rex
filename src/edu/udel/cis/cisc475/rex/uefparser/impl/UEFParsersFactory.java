49e0f8a19b5f4bfb6c877021144b23580b99126c
elhassa <elhassa@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 02:18:28 +0000

    Add UEFParserFactoryIF implementation

---
+++ src/edu/udel/cis/cisc475/rex/uefparser/impl/UEFParsersFactory.java
@@ -0,0 +1,21 @@
+package edu.udel.cis.cisc475.rex.uefparser.impl;
+
+import edu.udel.cis.cisc475.rex.uefparser.IF.UEFParserFactoryIF;
+import edu.udel.cis.cisc475.rex.uefparser.IF.UEFParserIF;
+
+/**
+ * This class implments UEFParserFactoryIF and is used to create instances of
+ * UEFParser
+ * 
+ * @author Ahmed El-Hassany
+ * 
+ */
+public class UEFParsersFactory implements UEFParserFactoryIF {
+
+	@Override
+	public UEFParserIF newUEFParser() {
+		UEFParser p = new UEFParser();
+		return p;
+	}
+
+}
