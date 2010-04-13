ae75848d4da97fb3fda442a294f65e63bef9b8b3
iburns <iburns@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 02:13:51 +0000

    Corrected imports.

--- test-output/ExamWriterTest.java
+++ test-output/ExamWriterTest.java
@@ -1,10 +1,10 @@
 package edu.udel.cis.cisc475.rex.output;
 
-import java.io.PrintWriter;
-import java.util.Collection;
+import static org.junit.Assert.*;
 
 import edu.udel.cis.cisc475.rex.output.ExamWriter;
-import static org.junit.Assert.*;
+
+
 
 
 public class ExamWriterTest {
