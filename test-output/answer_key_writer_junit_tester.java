3ef186bec9f57c7e352fba96d7dc96db5d8590a0
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 00:27:14 +0000

    added answer key writer junit tester shell

---
+++ test-output/answer_key_writer_junit_tester.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import edu.udel.cis.cisc475.rex.key.impl.Key;
+import edu.udel.cis.cisc475.rex.output.AnswerKeyWriter;
+/**
+ * @author cardona
+ *
+ */
+public class answer_key_writer_junit_tester {
+
+	
+	
+	@Test
+	public void test_answer_key_writer() {	
+	//declare a pointer to an interface and
+	//create an object of the interface type
+	Key K = new Key ("version string name", "exam string name ", "date string");		
+	
+ 	//create temp containers
+	Collection <String> Answer_a = new ArrayList <String>(); 
+	Answer_a.add(new String("B"));
+	//more than one correct answer
+	Collection <String> Answer_b = new ArrayList <String>(); 
+	Answer_b.add(new String("A"));
+	Answer_b.add(new String("C"));
+	Collection <String> Answer_c = new ArrayList <String>(); 
+	Answer_c.add(new String("B"));
+	
+	//add problems to our class from the containers
+ 	K.addProblem(Answer_a);	 	
+ 	K.addProblem(Answer_b);	 	
+ 	K.addProblem(Answer_c);	 	
+
+ 	//create the writer only once the key is built??
+ 	AnswerKeyWriter AKW = new AnswerKeyWriter (K);		
+
+ 	//create output stream
+ 	PrintWriter pw = new PrintWriter(System.out,true); 
+	 
+ 	//make call to write out the answer key
+ 	AKW.write(pw);
+ 	
+}//end test
+}//end of class 
