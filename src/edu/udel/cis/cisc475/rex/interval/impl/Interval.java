f6c18d28fbcf6b33083412d30b4ba0ac582b7ad4
bouchard <bouchard@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 02:38:12 +0000

    First upload of Interval.java, all functions implemented

---
+++ src/edu/udel/cis/cisc475/rex/interval/impl/Interval.java
@@ -0,0 +1,42 @@
+package edu.udel.cis.cisc475.rex.interval.impl;
+
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
+
+/*
+ * @author team 2
+ *
+ */
+
+public class Interval implements IntervalIF
+{
+	private Double low;
+	private boolean strictLow;
+	private Double high;
+	private boolean strictHigh;
+		
+	public Double low(){return low;}
+	public boolean strictLow(){return strictLow;}
+	public Double high(){return high;}
+	public boolean strictHigh(){return strictHigh;}
+
+	/* when "high" is 5: 
+	 * strictHigh: x < 5
+	 * !strictHigh: x <= 5 */
+	
+	public boolean contains(double value)
+	{
+		/* tests for outright out of range */
+		if((value > high) || (value < low)){return false;}
+			
+		/* if strictLow is on, x > low, so if x is low, false */
+		if(strictLow && (x == low)){return false;}
+		
+		/* if strictHigh is on, x < high, so if x is high, false */
+		if(strictHigh && (x == high)){return false;}
+		
+		/*otherwise, the value is inside the range and can be 
+		 * equal to high and low so return true */
+		return true;
+	}
+
+}
