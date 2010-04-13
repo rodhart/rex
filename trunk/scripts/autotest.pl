63eba2a864f624635e0b4515f22ebeb2c7f2a138
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 18:55:51 +0000

    Fixed javadoc script to include only files ending in .java

--- trunk/scripts/autotest.pl
+++ trunk/scripts/autotest.pl
@@ -1,6 +1,8 @@
 #!/usr/local/bin/perl
 
 $home = "/usa/siegel/tmp";
+$ant = "/usr/local/ant/bin/ant";
+
 $maindir = "rex-test";
 
 chdir($home) || die "Could not chdir $home";
@@ -11,9 +13,9 @@ $cmd = "svn checkout file:///home/www/repos/cisc475/trunk $maindir";
 system($cmd);
 chdir("$home/$maindir") || die "Could not chdir $home/maindir";
 system("cp build-configs/build.properties-cisc475 build.properties");
-system("ant test");
+system("$ant test");
 system("cp -r junit/reports /home/www/cisc475/htdocs/junit");
 
-system("ant javadoc");
+system("$ant javadoc");
 system("scp -r doc/javadoc/* /home/www/cisc475/htdocs/api");
 1;
