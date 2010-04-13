6cd5232a2e2a58fc45690a26a13e56a80d62b58f
mcloughl <mcloughl@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 04:54:21 +0000

    Added Stub for proper main module compiling.

--- trunk/src/edu/udel/cis/cisc475/rex/ecfparser/impl/EcfParserFactory.java
+++ trunk/src/edu/udel/cis/cisc475/rex/ecfparser/impl/EcfParserFactory.java
@@ -2,12 +2,13 @@ package edu.udel.cis.cisc475.rex.ecfparser.impl;
 
 import edu.udel.cis.cisc475.rex.ecfparser.IF.EcfParserFactoryIF;
 import edu.udel.cis.cisc475.rex.ecfparser.IF.EcfParserIF;
+
 /**
  * 
  * @author mcloughl
  * added stubs to make main compile
  */
-class EcfParserFactory implements EcfParserFactoryIF {
+public class EcfParserFactory implements EcfParserFactoryIF {
 
 	@Override
 	public EcfParserIF newParser(int numGeneratedExams) {
