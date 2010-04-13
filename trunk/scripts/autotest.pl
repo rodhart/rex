022240585c65c7672b2fa261a8c35b55c244b192
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 19:42:26 +0000

    Fixing bugs in script to capture output.

--- trunk/scripts/autotest.pl
+++ trunk/scripts/autotest.pl
@@ -4,8 +4,9 @@
 
 $home = "/usa/siegel/tmp";
 $ant = "/usr/local/ant/bin/ant";
-$out = "build.out";
-$err = "build.err";
+
+$out = "$home/build.out";
+$err = "$home/build.err";
 
 $maindir = "rex-test";
 
@@ -16,18 +17,18 @@ if (-e $maindir) {
 $cmd = "svn checkout file:///home/www/repos/cisc475/trunk $maindir";
 system("$cmd >$out 2>$err");
 chdir("$home/$maindir") || die "Could not chdir $home/maindir";
-system("cp build-configs/build.properties-cisc475 build.properties >>$out 2>>$err");
+system("cp build-configs/build.properties-cisc475 build.properties");
 
-system("$ant test");
-system("cp -r junit/reports /home/www/cisc475/htdocs/junit >>$out 2>>$err");
+system("$ant test >>$out 2>>$err");
+system("cp -r junit/reports /home/www/cisc475/htdocs/junit");
 
-system("$ant javadoc");
-system("cp -r doc/javadoc/* /home/www/cisc475/htdocs/api >>$out 2>>$err");
+system("$ant javadoc >>$out 2>>$err");
+system("cp -r doc/javadoc/* /home/www/cisc475/htdocs/api");
 
-system("$ant cobertura");
-system("cp -r cobertura/reports /home/www/cisc475/htdocs/cobertura >>$out 2>>$err");
+system("$ant cobertura >>$out 2>>$err");
+system("cp -r cobertura/reports /home/www/cisc475/htdocs/cobertura");
 
-system("cp $out /home/www/cisc475/htdocs");
-system("cp $err /home/www/cisc475/htdocs");
+system("cp $out /home/www/cisc475/htdocs/build.out");
+system("cp $err /home/www/cisc475/htdocs/buiod.err");
 
 1;
