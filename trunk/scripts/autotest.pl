1b1ec4e6a27325c1c75d5788eb988f02001ce769
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 19:13:42 +0000

    Updated scripts to handle Cobertura coverage analysis reports.

--- trunk/scripts/autotest.pl
+++ trunk/scripts/autotest.pl
@@ -13,9 +13,14 @@ $cmd = "svn checkout file:///home/www/repos/cisc475/trunk $maindir";
 system($cmd);
 chdir("$home/$maindir") || die "Could not chdir $home/maindir";
 system("cp build-configs/build.properties-cisc475 build.properties");
+
 system("$ant test");
 system("cp -r junit/reports /home/www/cisc475/htdocs/junit");
 
 system("$ant javadoc");
 system("scp -r doc/javadoc/* /home/www/cisc475/htdocs/api");
+
+system("$ant cobertura");
+system("scp -r cobertura/reports /home/www/cisc475/htdocs/cobertura");
+
 1;
