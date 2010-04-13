ff7863d31216a390d9e9fa3fcaebf13c8a0f2344
iburns <iburns@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 23:00:23 +0000

    Corrected test-output file names.

--- test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
+++ test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
@@ -1,23 +1,21 @@
 //package edu.udel.cis.cisc475.rex.output;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
 
+import edu.udel.cis.cisc475.rex.exam.impl.Exam;
 import edu.udel.cis.cisc475.rex.output.ExamWriter;
 
-// DOES NOT COMPILE
-
-
-
-public class ExamWriterTest {
-
+public class ExamWriterTest2 {
 	
-//	public void testExamWriterNull {
-//		
-//		Exam e = new Exam(true);
-//		
-//		ExamWriter ew = new ExamWriter(e);
-//		
-//		assertNotNull(ew);	
-//	}
+    @Test
+    public void testExamWriterNull() {
+		
+		Exam e = new Exam(true);
+		
+		ExamWriter ew = new ExamWriter(e);
+		
+		assertNotNull(ew);	
+	}
 	
 }
