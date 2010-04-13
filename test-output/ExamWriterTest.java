04114100695c007b6404d0a2fce14bbc9dd02d38
iburns <iburns@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 02:11:18 +0000

    Changed imports

--- test-output/ExamWriterTest.java
+++ test-output/ExamWriterTest.java
@@ -3,10 +3,7 @@ package edu.udel.cis.cisc475.rex.output;
 import java.io.PrintWriter;
 import java.util.Collection;
 
-import edu.udel.cis.cisc475.rex.output.IF.ExamWriterIF;
-import edu.udel.cis.cisc475.rex.exam.impl.Exam;
-import edu.udel.cis.cisc475.rex.exam.IF.ExamElementIF;
-
+import edu.udel.cis.cisc475.rex.output.ExamWriter;
 import static org.junit.Assert.*;
 
 
