85f43fe1bb8a094137db4d3f92f3babf1a9f68c2
mcloughl <mcloughl@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-13 04:33:39 +0000

    Added stub to allow main to use EcfParserFactory

---
+++ src/edu/udel/cis/cisc475/rex/ecfparser/impl/EcfParserFactory.java
@@ -0,0 +1,18 @@
+package edu.udel.cis.cisc475.rex.ecfparser.impl;
+
+import edu.udel.cis.cisc475.rex.ecfparser.IF.EcfParserFactoryIF;
+import edu.udel.cis.cisc475.rex.ecfparser.IF.EcfParserIF;
+/**
+ * 
+ * @author mcloughl
+ * added stubs to make main compile
+ */
+class EcfParserFactory implements EcfParserFactoryIF {
+
+	@Override
+	public EcfParserIF newParser(int numGeneratedExams) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
