14b4b76d1422ff693ebf911ed9eda9595d6f6bd3
gledhill <gledhill@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 20:24:23 +0000

    Added tests to ConfigTest.java. All tests pass.

--- trunk/test-config/edu/udel/cis/cisc475/rex/config/ConfigTest.java
+++ trunk/test-config/edu/udel/cis/cisc475/rex/config/ConfigTest.java
@@ -1,5 +1,8 @@
+
+
 package edu.udel.cis.cisc475.rex.config;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 import junit.framework.TestCase;
 
@@ -7,9 +10,74 @@ import edu.udel.cis.cisc475.rex.config.IF.ConfigFactoryIF;
 import edu.udel.cis.cisc475.rex.config.IF.ConfigIF;
 import edu.udel.cis.cisc475.rex.config.impl.ConfigFactory;
 import edu.udel.cis.cisc475.rex.config.impl.Config;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalFactoryIF;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
+import edu.udel.cis.cisc475.rex.interval.impl.IntervalFactory;
+
+/*
+ * @author Jesse Gledhill
+ */
+
 
 public class ConfigTest {
 	/*
-	 * Useful tests here
+	 * Tests that pdfOption() returns the correct boolean when pdfOption is true
 	 */
+	@Test
+	public void testPdfOptionTrue() {
+		ConfigFactoryIF configFactory = new ConfigFactory();
+		ConfigIF C = configFactory.newConfig(true, 5);
+		
+		assertTrue(C.pdfOption());
+	}
+
+	/*
+	 * Tests that pdfOption() returns the correct boolean when pdfOption is false
+	 */
+	@Test
+	public void testPdfOptionFalse() {
+		ConfigFactoryIF configFactory = new ConfigFactory();
+		ConfigIF C = configFactory.newConfig(false, 10);
+		
+		assertFalse(C.pdfOption());
+	}
+	
+	/*
+	 * Tests that numVersions() returns the correct integer when the number of versions is a single
+	 * digit number.
+	 */
+	@Test
+	public void testNumVersionsSingleD() {
+		ConfigFactoryIF configFactory = new ConfigFactory();
+		ConfigIF C = configFactory.newConfig(false, 1);
+		
+		assertEquals(C.numVersions(), 1);
+	}
+	
+	/*
+	 * Tests that numVersions() returns the correct integer when the number of versions is a 
+	 * negative number.
+	 */
+	@Test
+	public void testNumVersionsNegative() {
+		ConfigFactoryIF configFactory = new ConfigFactory();
+		ConfigIF C = configFactory.newConfig(false, -123);
+		
+		assertEquals(C.numVersions(), -123);
+	}
+	
+	/*
+	 * Tests that numVersions() returns the correct integer when the number of versions is a 
+	 * 4 digit number.
+	 */
+	@Test
+	public void testNumVersionsFourDigit() {
+		ConfigFactoryIF configFactory = new ConfigFactory();
+		ConfigIF C = configFactory.newConfig(false, 9999);
+		
+		assertEquals(C.numVersions(), 9999);
+	}
+	
+	
+	
 }
