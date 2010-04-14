1dc1110e1c2d3d60f95bc843664f0a3903a0f347
fitzpatr <fitzpatr@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 20:22:14 +0000

    Fixed some problems in the unit tests: ExamTest and ProblemTest. Removed includes and variables that weren't being used to avoid Eclipse warning of errors.

--- trunk/test-exam/edu/udel/cis/cisc475/rex/exam/ProblemTest.java
+++ trunk/test-exam/edu/udel/cis/cisc475/rex/exam/ProblemTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Collection;
