67fb1b75ac9d720b8054a9a82d6a47df6b458aac
rothhaup <rothhaup@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 00:04:13 +0000

    Starting point for the Config tests

---
+++ trunk/test-config/edu/udel/cis/cisc475/rex/config/ConfigTest.java
@@ -0,0 +1,15 @@
+package edu.udel.cis.cisc475.rex.config;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import edu.udel.cis.cisc475.rex.config.IF.ConfigFactoryIF;
+import edu.udel.cis.cisc475.rex.config.IF.ConfigIF;
+import edu.udel.cis.cisc475.rex.config.impl.ConfigFactory;
+import edu.udel.cis.cisc475.rex.config.impl.Config;
+
+public class ConfigTest {
+	/*
+	 * Useful tests here
+	 */
+}
