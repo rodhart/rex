76df4237a2d8dfda6f897db545b3f9e682f7d1fa
gledhill <gledhill@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 21:29:53 +0000

    Added Group constraint tests which are part of the config package. All tests pass. Also added two text files which are used by GroupConstraintTest.java.

---
+++ test-config/edu/udel/cis/cisc475/rex/config/GroupConstraintTest.java
@@ -0,0 +1,232 @@
+package edu.udel.cis.cisc475.rex.config;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import edu.udel.cis.cisc475.rex.config.IF.GroupConstraintIF;
+import edu.udel.cis.cisc475.rex.config.impl.GroupConstraint;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalFactoryIF;
+import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
+import edu.udel.cis.cisc475.rex.interval.impl.IntervalFactory;
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
+
+/*
+ * @author Jesse Gledhill
+ */
+
+
+public class GroupConstraintTest {
+	
+	/*
+	 * Tests that difficultyInterval() returns the correct interval from groupConstraint.
+	 */
+	@Test
+	public void testDifficultyInterval() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		IntervalIF J = GroupConstraint.difficultyInterval();
+		
+		assertTrue(I.strictLow() == false);
+		assertTrue(I.strictHigh() == true);
+		assertTrue(I.low() == 5.0);
+		assertTrue(I.high() == 10.0);
+	}
+	
+	/*
+	 * Second Test: Tests that difficultyInterval() returns the correct interval from groupConstraint.
+	 */
+	@Test
+	public void testDifficultyInterval2() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(true, 50.5, false, 100.2);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		IntervalIF J = GroupConstraint.difficultyInterval();
+		
+		assertTrue(I.strictLow() == true);
+		assertTrue(I.strictHigh() == false);
+		assertTrue(I.low() == 50.5);
+		assertTrue(I.high() == 100.2);
+	}
+	
+	/*
+	 * Tests that numProblems() returns the correct integer from groupConstraint.
+	 */
+	@Test
+	public void testNumProblems() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		assertEquals(GroupConstraint.numProblems(), 10);
+	
+	}
+	
+	/*
+	 * Second Test: Tests that numProblems() returns the correct integer from groupConstraint.
+	 */
+	@Test
+	public void testNumProblems2() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 5555, 20, "testing", S);
+		
+		assertEquals(GroupConstraint.numProblems(), 5555);
+	
+	}
+	
+	/*
+	 * Tests that source() returns the correct source from groupConstraint.
+	 */
+	@Test
+	public void testSource() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt");
+	
+	}
+	
+	/*
+	 * Second Test: Tests that source() returns the correct source from groupConstraint.
+	 */
+	@Test
+	public void testSource2() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt");
+	
+	}
+	
+	/*
+	 * Tests that points() returns the correct integer from groupConstraint.
+	 */
+	@Test
+	public void testPoints() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(GroupConstraint.points(), 20);
+	
+	}
+	
+	/*
+	 * Second Test: Tests that points() returns the correct integer from groupConstraint.
+	 */
+	@Test
+	public void testPoints2() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20222, "testing", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(GroupConstraint.points(), 20222);
+	
+	}
+	
+	/*
+	 * Tests that topic() returns the correct string from groupConstraint.
+	 */
+	@Test
+	public void testTopic() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "testing", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(GroupConstraint.topic(), "testing");
+	
+	}
+	
+	/*
+	 * Second Test: Tests that topic() returns the correct string from groupConstraint.
+	 */
+	@Test
+	public void testTopic2() {
+		IntervalFactoryIF intervalFactory = new IntervalFactory();
+		IntervalIF I = intervalFactory.interval(false, 5.0, true, 10.0);
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		GroupConstraintIF GroupConstraint = new GroupConstraint(I, 10, 20, "-8test33ing2", S);
+		
+		SourceIF F = GroupConstraint.source();
+		
+		assertEquals(GroupConstraint.topic(), "-8test33ing2");
+	
+	}
+}
