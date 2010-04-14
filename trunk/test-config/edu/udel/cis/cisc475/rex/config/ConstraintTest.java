7d50d515cc8caf1a145406be3302e587bd1ce0da
gledhill <gledhill@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 22:10:38 +0000

    Added Constraint tests which are part of the config package. All tests pass.

---
+++ trunk/test-config/edu/udel/cis/cisc475/rex/config/ConstraintTest.java
@@ -0,0 +1,59 @@
+package edu.udel.cis.cisc475.rex.config;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import edu.udel.cis.cisc475.rex.config.IF.ConstraintIF;
+import edu.udel.cis.cisc475.rex.config.impl.Constraint;
+
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
+
+/*
+ * @author Jesse Gledhill
+ */
+
+
+public class ConstraintTest {
+	
+	/*
+	 * Tests that source() returns the correct filename from Constraint.
+	 */
+	@Test
+	public void testSource() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		ConstraintIF Constraint = new Constraint(S);
+		
+		SourceIF F	= Constraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt");
+	
+	}
+	
+	/*
+	 * Second Test: Tests that source() returns the correct filename from Constraint.
+	 */
+	@Test
+	public void testSource2() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		ConstraintIF Constraint = new Constraint(S);
+		
+		SourceIF F	= Constraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt");
+	
+	}
+	
+}
