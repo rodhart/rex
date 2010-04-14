86d96df71cb3df93d981e6951cbc025700b63075
schultz <schultz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 03:16:56 +0000

    Added try/catch block to prevent errors being thrown when running JUnit tests

--- src/edu/udel/cis/cisc475/rex/generate/impl/GeneratorFactory.java
+++ src/edu/udel/cis/cisc475/rex/generate/impl/GeneratorFactory.java
@@ -1,15 +1,20 @@
 package edu.udel.cis.cisc475.rex.generate.impl;
 
 import edu.udel.cis.cisc475.rex.config.IF.ConfigIF;
+import edu.udel.cis.cisc475.rex.err.RexUnsatisfiableException;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorFactoryIF;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorIF;
 
 public class GeneratorFactory implements GeneratorFactoryIF {
 
-	@Override
 	public GeneratorIF newGenerator(ExamIF master, ConfigIF config) {
-		return new Generator(master, config);
+		try {
+			return new Generator(master, config);
+		} catch (RexUnsatisfiableException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
