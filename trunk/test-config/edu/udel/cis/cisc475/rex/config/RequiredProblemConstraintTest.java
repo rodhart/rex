0958f0d18e791a7eea9866175def2acbaa283db9
gledhill <gledhill@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 21:58:50 +0000

    Added Required Problem Constraint tests which are part of the config package. All tests pass.

---
+++ trunk/test-config/edu/udel/cis/cisc475/rex/config/RequiredProblemConstraintTest.java
@@ -0,0 +1,127 @@
+package edu.udel.cis.cisc475.rex.config;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import edu.udel.cis.cisc475.rex.config.IF.RequiredProblemConstraintIF;
+import edu.udel.cis.cisc475.rex.config.impl.RequiredProblemConstraint;
+
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
+
+/*
+ * @author Jesse Gledhill
+ */
+
+
+public class RequiredProblemConstraintTest {
+	
+	/*
+	 * Tests that label() returns the correct string from RequiredProblem.
+	 */
+	@Test
+	public void testLabel() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("testing", 10, S);
+		
+		assertEquals(RequiredProblemConstraint.label(), "testing");
+	
+	}
+	
+	/*
+	 * Second Test: Tests that label() returns the correct string from RequiredProblem.
+	 */
+	@Test
+	public void testLabel2() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("32423testing000", 10, S);
+		
+		assertEquals(RequiredProblemConstraint.label(), "32423testing000");
+	
+	}
+	
+	/*
+	 * Tests that points() returns the correct integer from RequiredProblem.
+	 */
+	@Test
+	public void testPoints() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("32423testing000", 10, S);
+		
+		assertEquals(RequiredProblemConstraint.points(), 10);
+	
+	}
+	
+	/*
+	 * Second Test: Tests that points() returns the correct integer from RequiredProblem.
+	 */
+	@Test
+	public void testPoints2() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("32423testing000", 999310, S);
+		
+		assertEquals(RequiredProblemConstraint.points(), 999310);
+	
+	}
+	
+	/*
+	 * Tests that source() returns the correct filename from RequiredProblem.
+	 */
+	@Test
+	public void testSource() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("32423testing000", 999310, S);
+		
+		SourceIF F	= RequiredProblemConstraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText.txt");
+	
+	}
+	
+	/*
+	 * Second Test: Tests that source() returns the correct filename from RequiredProblem.
+	 */
+	@Test
+	public void testSource2() {
+		
+		String filename = "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt";
+		
+		SourceFactoryIF sourceFactory = new SourceFactory();
+		SourceIF S = sourceFactory.newSource(filename);
+		
+		RequiredProblemConstraintIF RequiredProblemConstraint = new RequiredProblemConstraint("32423testing000", 999310, S);
+		
+		SourceIF F	= RequiredProblemConstraint.source();
+		
+		assertEquals(F.filename(), "./trunk/test-config/edu/udel/cis/cisc475/rex/config/ExampleText2.txt");
+	
+	}
+	
+}
