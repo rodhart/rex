901836023609bbdcdd9bd76b08149ac181ae328f
simons <simons@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-14 07:57:36 +0000

    Further defined generate() in class Generator. Fixed a few minor bugs, and found one or two minor flaws in our design that are impeding our progress.

--- src/edu/udel/cis/cisc475/rex/generate/IF/GeneratorFactoryIF.java
+++ src/edu/udel/cis/cisc475/rex/generate/IF/GeneratorFactoryIF.java
@@ -1,12 +1,14 @@
 package edu.udel.cis.cisc475.rex.generate.IF;
 
 import edu.udel.cis.cisc475.rex.config.IF.*;
+import edu.udel.cis.cisc475.rex.err.RexUnsatisfiableException;
 import edu.udel.cis.cisc475.rex.exam.IF.*;
 
 public interface GeneratorFactoryIF {
 	
 	/**
 	 * @author Team 3
+	 * 
 	 */
 
 	GeneratorIF newGenerator(ExamIF master, ConfigIF config);
