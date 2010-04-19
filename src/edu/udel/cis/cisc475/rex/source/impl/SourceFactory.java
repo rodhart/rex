2f50f144369df70ba675509a301b7ee05239be12
jsong <jsong@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-19 23:11:29 +0000

    Cleaned up old code, removed comments, reorganized

--- src/edu/udel/cis/cisc475/rex/source/impl/SourceFactory.java
+++ src/edu/udel/cis/cisc475/rex/source/impl/SourceFactory.java
@@ -14,30 +14,12 @@ public class SourceFactory implements SourceFactoryIF {
 	/**
 	 *  Creates a new instance of Source 
 	 *  
-	 *  @param filename File to be used
-	 *  @return Returns a new Source object with the file parsed
+	 *  @param filename Name of file
+	 *  @return Returns a new Source object with the given file name
 	 *  
 	 */
 	public SourceIF newSource(String filename) {
 		return new Source(filename);
 	}
 
-
-	/**
-	 *  Creates a new instance of Source 
-	 *  
-	 *  @param filename File to be used
-	 *  @param startLine  startLine to be used
-	 *  @param startColumn startColumn to be used
-	 *  @param lastLine lastLine to be used
-	 *  @param lastColumn lastColumn File to be used
-	 *  @return Returns a new Source object with the file parsed
-	 *  
-	 */
-/*	public SourceIF newSource(String filename, int startLine, int startColumn,
-								int lastLine, int lastColumn){
-
-		return new Source(filename,	startLine, startColumn, lastLine, lastColumn);
-	}//end of method
-*/
 }//end of class
