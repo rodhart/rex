3ae0bc380505bf9ed5a5dcb54fac33dd72d197a5
jsong <jsong@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-19 22:59:40 +0000

    Changed Source.java to reflect what was explained in class.  text() returns full body of the text.  write() outputs the text to PrintWriter

--- trunk/src/edu/udel/cis/cisc475/rex/source/impl/Source.java
+++ trunk/src/edu/udel/cis/cisc475/rex/source/impl/Source.java
@@ -17,11 +17,12 @@ import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
  * @author Jim Cardona
  * @author Justin Johnson
  * @author Jack Song
- * @version .3, 12 Apr 2010
+ * @version 1.0, 19 Apr 2010
  *
  */
 public class Source implements SourceIF {
 
+	public static String newline = System.getProperty("line.separator");
 /**
  * Constructor for the class
  * 
@@ -30,16 +31,11 @@ public class Source implements SourceIF {
 public Source(String filename) {
 	 //init private vars
      startline = startcolumn = lastline = lastcolumn = 0;	
-	 Text = " "; //using this as temp string buffer only
-
-    // create File object based on user input
+	 Text = ""; 
 	 fileName = filename;	
-	 name = new File( fileName );
+	 myText = new StringBuffer();
+	// name = new File( fileName );
 	
-	 //array list object that will contain all of our lines of imported text
-	 text_lines = new ArrayList<String>();
-	 
-	 
 	 /*	check if the filename is an existing file (need try - catch statements) 
 	  * 
 	 	this instance of source is being used to read in
@@ -50,7 +46,7 @@ public Source(String filename) {
 	 	and 
 	 	set the markers along the way	 	
 	 */
-     if ( name.exists() ) // check if exists
+     /*if ( name.exists() ) // check if exists
      { if ( name.isFile() ) // check if is file and not directory
      { 
 
@@ -83,7 +79,7 @@ public Source(String filename) {
      }//end of if ( name.exists() ) 
      	else {}
      	// exception file not found
-     
+     */
 }//end of constructor
 
 
@@ -198,23 +194,28 @@ public void write(PrintWriter out) {
 	//	the contents to out
 	// use delimiters set by requesting program of
 	//start and end rows
-	int size = text_lines.size();
-	
+	out.printf("%s\n", Text);
+/*	
 	String tempString = "";
+	
+	// Checks startline, lastline < 0 and startline > lastline cases
 	if(startline < 0 || lastline < 0 || startline > lastline)
 	{
 		System.err.println("Startline = " + startline);
 		System.err.println("Lastline = " + lastline);
 		System.err.println("Boundary error");
 	}
+	// Checks startline > length of file case, gives an error
 	else if(startline > size)
 	{
 		System.err.println("Startline is past the end of file");
 	}
+	// Checks lastline > lenth of file case, sets lastline to the end of file
 	else if(lastline > size)
 	{
 		lastline = size-1;
 	}
+	// startline = lastline case, prints that one line
 	else if(startline == lastline)
 	{
 		for(int i = startcolumn; i < text_lines.get(startline).length(); i++)
@@ -223,22 +224,24 @@ public void write(PrintWriter out) {
 		}
 		System.out.printf("%s\n", tempString);
 	}
-	
+	// normal conditions
 	else
 	{
+		// prints startline from startcolumn to endline
 		if(startcolumn < text_lines.get(startline).length())
 		{
 			for(int i = startcolumn; i<text_lines.get(startline).length(); i++)
 				tempString += text_lines.get(startline).charAt(i);
 			out.printf("%s\n", tempString);
 		}
-
+		// prints startline+1 to lastline-1
 		for (int i = startline + 1; (i < size) && (i < lastline - 1); i++)
 			out.printf("%s\n", text_lines.get(i)); 
 		tempString = "";
-
+		// no lastcolumn set, prints lastline completely
 		if(lastcolumn == 0)
 			out.printf("%s\n", text_lines.get(lastline));
+		// prints lastline from beginning to lastcolumn
 		else
 		{
 			for(int i = 0; (i<text_lines.get(lastline).length() && i < lastcolumn); i++)
@@ -246,7 +249,7 @@ public void write(PrintWriter out) {
 			out.printf("%s\n", tempString);
 		}
 	}
-} 
+*/} 
 
 // add text to our structure
 
@@ -256,18 +259,19 @@ public void write(PrintWriter out) {
  * @param text Text to be added
  */
 public void addText(String text) {
-	text_lines.add(text);
+	myText.append(text);
+	myText.append(newline);
+	Text = myText.toString();
 }
 
 	
 //private vars
 private String fileName;
-private File name;		//our file object handle
-private Scanner input;  //our input object handle 
-private String Text;	//using this as temp string buffer only
+private String Text;	
 private int startline;
 private int startcolumn;
 private int lastline;
 private int lastcolumn;	
-		List<String> text_lines; //our string array
+private StringBuffer myText;
+//		List<String> text_lines; //our string array
 }//end of class SourceFactoryIF 
