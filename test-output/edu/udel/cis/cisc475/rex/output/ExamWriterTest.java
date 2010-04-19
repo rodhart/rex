75d56fc0b25530589050cab5ddd61396a37f3a37
jsong <jsong@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-19 20:04:22 +0000

    Fixed declarations in ExamWriterTest

--- test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
+++ test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
@@ -1,20 +1,30 @@
 package edu.udel.cis.cisc475.rex.output;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.udel.cis.cisc475.rex.config.IF.ConfigFactoryIF;
 import edu.udel.cis.cisc475.rex.config.impl.ConfigFactory;
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
 import edu.udel.cis.cisc475.rex.exam.impl.Answer;
 import edu.udel.cis.cisc475.rex.exam.impl.Block;
 import edu.udel.cis.cisc475.rex.exam.impl.Exam;
+import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
 import edu.udel.cis.cisc475.rex.exam.impl.Figure;
 import edu.udel.cis.cisc475.rex.exam.impl.Problem;
 import edu.udel.cis.cisc475.rex.interval.IF.IntervalFactoryIF;
 import edu.udel.cis.cisc475.rex.interval.impl.IntervalFactory;
 import edu.udel.cis.cisc475.rex.output.ExamWriter;
+import edu.udel.cis.cisc475.rex.output.IF.ExamWriterIF;
+import edu.udel.cis.cisc475.rex.output.IF.OutputFactoryIF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
 import edu.udel.cis.cisc475.rex.source.impl.Source;
 import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
 
@@ -33,40 +43,40 @@ public class ExamWriterTest {
 	
     @Test
     public void testExamWriterNull() {
-		Exam e = examFactory.newGeneratedExam();
-		ExamWriter ew = examWriterFactory.newExamWriter(e);
+		ExamIF e = examFactory.newGeneratedExam();
+		ExamWriterIF ew = examWriterFactory.newExamWriter(e);
 		assertNotNull(ew);	
 	}
     
     @Test
     public void testPrintBlockIF(){
-    	Exam e = examFactory.newGeneratedExam();	
-    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Block blockTest = examFactory.newBlock("test topic", "test label", testSource );
+    	ExamIF e = examFactory.newGeneratedExam();	
+    	SourceIF testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	BlockIF blockTest = examFactory.newBlock("test topic", "test label", testSource );
     	e.addElementIF(blockTest);
-    	ExamWriter ew = examWriterFactory.newExamWriter(e);
+    	ExamWriterIF ew = examWriterFactory.newExamWriter(e);
     }
     
     @Test
     public void testPrintFigureIF(){
-    	Exam e = examFactory.newGeneratedExam();	
-    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Figure figureTest = examFactory.newFigure("test label", testSource );
+    	ExamIF e = examFactory.newGeneratedExam();	
+    	SourceIF testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	FigureIF figureTest = examFactory.newFigure("test label", testSource );
     	e.addElementIF(figureTest);
-    	ExamWriter ew = examWriterFactory.newExamWriter(e);
+    	ExamWriterIF ew = examWriterFactory.newExamWriter(e);
     }
     
     @Test
     public void testPrintProblem(){
-    	Exam e = examFactory.newGeneratedExam();	
-    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Answer answerTest = examFactory.newAnswer(true, testSource);
-    	Answer answerTest2 = examFactory.newAnswer(false, testSource);
-    	Answer[] answerArrayTest = {answerTest, answerTest2};
+    	ExamIF e = examFactory.newGeneratedExam();	
+    	SourceIF testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	AnswerIF answerTest = examFactory.newAnswer(true, testSource);
+    	AnswerIF answerTest2 = examFactory.newAnswer(false, testSource);
+    	AnswerIF[] answerArrayTest = {answerTest, answerTest2};
     	
     	//Problem problemTest = new Problem("test topic", "test label", testSource, answerArrayTest );
     	//e.addElementIF(problemTest);
-    	ExamWriter ew = examWriterFactory.newExamWriter(e);    
+    	ExamWriterIF ew = examWriterFactory.newExamWriter(e);    
     }
     
 }
