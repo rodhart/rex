0852e80aca3204a622314e22adfe4d34055ee8b1
kiernan <kiernan@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-19 19:52:03 +0000

    added the @before to our junit test and formatted the rest of the tests accordingly

--- trunk/test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
+++ trunk/test-output/edu/udel/cis/cisc475/rex/output/ExamWriterTest.java
@@ -3,57 +3,63 @@ package edu.udel.cis.cisc475.rex.output;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import edu.udel.cis.cisc475.rex.config.impl.ConfigFactory;
 import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
 import edu.udel.cis.cisc475.rex.exam.impl.Answer;
 import edu.udel.cis.cisc475.rex.exam.impl.Block;
 import edu.udel.cis.cisc475.rex.exam.impl.Exam;
 import edu.udel.cis.cisc475.rex.exam.impl.Figure;
 import edu.udel.cis.cisc475.rex.exam.impl.Problem;
+import edu.udel.cis.cisc475.rex.interval.impl.IntervalFactory;
 import edu.udel.cis.cisc475.rex.output.ExamWriter;
 import edu.udel.cis.cisc475.rex.source.impl.Source;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
 
 public class ExamWriterTest {
 	
+	@Before
+	public void setUp() {
+		examFactory = new ExamFactory();
+	    sourceFactory = new SourceFactory();
+	    examWriterFactory = new OutputFactory();
+	}
+	
     @Test
     public void testExamWriterNull() {
-		
-		Exam e = new Exam(true);
-		
-		ExamWriter ew = new ExamWriter(e);
-		
+		Exam e = examFactory.newGeneratedExam();
+		ExamWriter ew = examWriterFactory.newExamWriter(e);
 		assertNotNull(ew);	
 	}
     
     @Test
     public void testPrintBlockIF(){
-    	Exam e = new Exam(true);	
-    	Source testSource = new Source("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Block blockTest = new Block("test topic", "test label", testSource );
+    	Exam e = examFactory.newGeneratedExam();	
+    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	Block blockTest = examFactory.newBlock("test topic", "test label", testSource );
     	e.addElementIF(blockTest);
-    	ExamWriter ew = new ExamWriter(e);
+    	ExamWriter ew = examWriterFactory.newExamWriter(e);
     }
     
     @Test
     public void testPrintFigureIF(){
-    	Exam e = new Exam(true);	
-    	Source testSource = new Source("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Figure figureTest= new Figure("test label", testSource );
+    	Exam e = examFactory.newGeneratedExam();	
+    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	Figure figureTest = examFactory.newFigure("test label", testSource );
     	e.addElementIF(figureTest);
-    	ExamWriter ew = new ExamWriter(e);
+    	ExamWriter ew = examWriterFactory.newExamWriter(e);
     }
     
     @Test
     public void testPrintProblem(){
-    	Exam e = new Exam(true);	
-    	Source testSource = new Source("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
-    	Answer answerTest = new Answer(true, testSource);
-    	Answer answerTest2 = new Answer(false, testSource);
+    	Exam e = examFactory.newGeneratedExam();	
+    	Source testSource = sourceFactory.newSource("./trunk/test-source/edu/udel/cis/cisc475/rex/source/SampleText.txt");
+    	Answer answerTest = examFactory.newAnswer(true, testSource);
+    	Answer answerTest2 = examFactory.newAnswer(false, testSource);
     	Answer[] answerArrayTest = {answerTest, answerTest2};
     	
     	//Problem problemTest = new Problem("test topic", "test label", testSource, answerArrayTest );
     	//e.addElementIF(problemTest);
-    	ExamWriter ew = new ExamWriter(e);
-    
+    	ExamWriter ew = examWriterFactory.newExamWriter(e);    
     }
-	
+    
 }
