11828acb28318d9312464a0abb95273e5ceca086
krapf <krapf@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-20 04:31:26 +0000

    Created source stubs for generate JUnit testing.

---
+++ test-generate/edu/udel/cis/cisc475/rex/source/generatestubs/SourceStub.java
@@ -0,0 +1,97 @@
+package edu.udel.cis.cisc475.rex.source.generatestubs;
+
+import java.io.PrintWriter;
+
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+
+
+/**
+ * @author krapf
+ *
+ */
+public class SourceStub implements SourceIF {
+	String s_filename;
+	String s_text;
+	int s_startLine = 0;
+	int s_startColumn = 0;
+	int s_lastLine = 0;
+	int s_lastColumn = 0;
+	
+	
+	public SourceStub(String filename) {
+		this.s_filename = filename;
+	}
+
+	@Override
+	public void addText(String text) {
+		// TODO Auto-generated method stub
+		this.s_text = text;
+	}
+
+	@Override
+	public String filename() {
+		// TODO Auto-generated method stub
+		return s_filename;
+	}
+
+	@Override
+	public int lastColumn() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLastColumn(int column) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLastLine(int line) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStartColumn(int column) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStartLine(int line) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int startColumn() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int startLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String text() {
+		// TODO Auto-generated method stub
+		return s_text;
+	}
+
+	@Override
+	public void write(PrintWriter out) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
