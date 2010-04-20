c1e7827dd5dc8f8c0da21e8e45a1aa981cf24115
fitzpatr <fitzpatr@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-20 02:53:54 +0000

    Added ExamElement test

---
+++ trunk/test-exam/edu/udel/cis/cisc475/rex/exam/ExamElementTest.java
@@ -0,0 +1,55 @@
+/**
+ * rex
+ * edu.udel.cis.cisc475.rex.exam
+ * Test Suite for testing Answer methods.
+ * Apr 9, 2010
+ * hboyd
+ */
+package edu.udel.cis.cisc475.rex.exam;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.udel.cis.cisc475.rex.exam.impl.ExamElement;
+
+
+/**
+ * @author fxfitz
+ *
+ */
+public class ExamElementTest {
+	
+	static ExamElement ee = null;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ee = new ExamElement("physics");
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testExamElement(){
+		// Check to make sure it was created.
+		assertNotNull(ee);
+		
+		// Make sure it was set to physics
+		assertEquals("physics", ee.label());
+	}
+}
