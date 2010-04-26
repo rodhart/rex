990007fb5b2b5c86271bb4ac042e8b1d3dcaf9f0
wgordon <wgordon@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-26 00:38:59 +0000

    Added stubs for testing

--- test-generate/edu/udel/cis/cisc475/rex/exam/generatestubs/ExamStub.java
+++ test-generate/edu/udel/cis/cisc475/rex/exam/generatestubs/ExamStub.java
@@ -1,17 +1,34 @@
 package edu.udel.cis.cisc475.rex.exam.generatestubs;
 
+import java.awt.List;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
 import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamElementIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
+import edu.udel.cis.cisc475.rex.exam.impl.Answer;
+import edu.udel.cis.cisc475.rex.exam.impl.Block;
 import edu.udel.cis.cisc475.rex.exam.impl.Problem;
+import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.generatestubs.SourceFactoryStub;
 
 public class ExamStub implements ExamIF {
 
+	private boolean isMaster = true;
+	//private HashSet<String> labels = new HashSet<String>();
+	//labels.add("A");
+	SourceFactoryIF sourceFactory = new SourceFactoryStub();
+	ExamFactoryIF examFactory = new ExamFactoryStub();
+	
+	
 	@Override
 	public int addElementIF(ExamElementIF element) {
 		return 0;
@@ -61,20 +78,26 @@ public class ExamStub implements ExamIF {
 
 	@Override
 	public BlockIF finalBlock() {
-		// TODO Auto-generated method stub
-		return null;
+		SourceIF finalSource = sourceFactory.newSource("testFile.txt");
+		finalSource.addText("This is the text for the final block");
+		
+		BlockIF finalBlock = examFactory.newBlock("Final topic", "Final label", finalSource);
+		
+		return finalBlock;
 	}
 
 	@Override
 	public SourceIF frontMatter() {
-		// TODO Auto-generated method stub
-		return null;
+		SourceIF frontMatter = sourceFactory.newSource("testFile.txt");
+		frontMatter.addText("First page of exam");
+		
+		return frontMatter;
 	}
 
 	@Override
 	public boolean isMaster() {
 		// TODO Auto-generated method stub
-		return false;
+		return isMaster;
 	}
 
 	@Override
@@ -91,15 +114,41 @@ public class ExamStub implements ExamIF {
 
 	@Override
 	public SourceIF preamble() {
-		// TODO Auto-generated method stub
-		return null;
+		SourceIF preamble = sourceFactory.newSource("testFile.txt");
+		preamble.addText("Intro to exam");
+		
+		return preamble;
 	}
 
 	@Override
 	public Collection<ProblemIF> problems() {
-		// TODO Auto-generated method stub
+		SourceIF question1 = sourceFactory.newSource("testFile.txt");
+		question1.addText("question1");
 		
-		return null;
+		AnswerIF[] answers1 = new Answer[2];
+		//answers1[0] = examFactory.newAnswer(true, sourceFactory.newSource("option1"));
+		//answers1[1] = examFactory.newAnswer(false, sourceFactory.newSource("option2"));
+		
+		answers1[0] = new Answer(true, sourceFactory.newSource("option1"));
+		answers1[1] = new Answer(false, sourceFactory.newSource("option2"));
+		
+		ProblemIF prob1 = examFactory.newProblem("topic1", "label1", question1, answers1);
+		
+		
+		SourceIF question2 = sourceFactory.newSource("testFile.txt");
+		question1.addText("question2");
+		
+		AnswerIF[] answers2 = new AnswerStub[2];
+		answers2[0] = examFactory.newAnswer(false, sourceFactory.newSource("option3"));
+		answers2[1] = examFactory.newAnswer(true, sourceFactory.newSource("option4"));
+		
+		ProblemIF prob2 = examFactory.newProblem("topic1", "label1", question2, answers2);
+		
+		ArrayList<ProblemIF> list = new ArrayList<ProblemIF>();
+		list.add(prob1);
+		list.add(prob2);
+		
+		return list;
 	}
 
 	@Override
