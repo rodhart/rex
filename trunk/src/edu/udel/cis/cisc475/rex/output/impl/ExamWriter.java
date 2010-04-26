abe4295d8747e124a495a5e7fbdb61aec9fb4715
kiernan <kiernan@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-26 21:47:28 +0000

    fixed formatting

--- trunk/src/edu/udel/cis/cisc475/rex/output/impl/ExamWriter.java
+++ trunk/src/edu/udel/cis/cisc475/rex/output/impl/ExamWriter.java
@@ -8,12 +8,13 @@ import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.IF.FigureIF;
 import edu.udel.cis.cisc475.rex.exam.impl.Problem;
 import edu.udel.cis.cisc475.rex.output.IF.ExamWriterIF;
+
 /**
- * The ExamWriter class utilizes a print writer and an precreated Exam
- * class to output parts of the exam class to the print writer.
- *  
- *  @author kiernan
- *  @author justinjo
+ * The ExamWriter class utilizes a print writer and an precreated Exam class to
+ * output parts of the exam class to the print writer.
+ * 
+ * @author kiernan
+ * @author justin
  */
 
 public class ExamWriter implements ExamWriterIF {
@@ -21,61 +22,60 @@ public class ExamWriter implements ExamWriterIF {
 	/**
 	 * Default constructor
 	 * 
-	 * @param exam an exam object with a preamble, front matter, and problems
+	 * @param exam
+	 *            an exam object with a preamble, front matter, and problems
 	 */
 	public ExamWriter(ExamIF exam) {
 		this.E = exam;
-		}//end of constructor
+	}// end of constructor
 
 	/**
-	 * Loops through each exam element and 
-	 * writes it to the PrintWriter object.
-	 * 	
-	 * @param out any writer used to output data
+	 * Loops through each exam element and writes it to the PrintWriter object.
+	 * 
+	 * @param out
+	 *            any writer used to output data
 	 */
-	public void write(PrintWriter out){
+	public void write(PrintWriter out) {
 		// output beginning of exam file
 		out.printf("%s\n", E.preamble());
 		out.printf("%s\n", E.frontMatter());
 
 		// output problems with respective answers
-		for(int i=0; i< E.elements().toArray().length; i++){
+		for (int i = 0; i < E.elements().toArray().length; i++) {
 			ExamElementIF temp = (ExamElementIF) E.elements().toArray()[i];
 
 			// the following checks what type the current element is
 			// and prints accordingly
-			if(temp instanceof BlockIF){
-				BlockIF tempBlockIF  = (BlockIF)temp;
+			if (temp instanceof BlockIF) {
+				BlockIF tempBlockIF = (BlockIF) temp;
 				out.printf("%s\n", tempBlockIF.source().text());
-				}
-			else if(temp instanceof FigureIF){
+			} else if (temp instanceof FigureIF) {
 				out.println("/newpage\n");
-				FigureIF tempFigureIF = (FigureIF)temp;
-				out.printf("%s\n",tempFigureIF.source().text());
-				}
-			else if(temp instanceof Problem){
-				Problem tempProblem = (Problem)temp;
+				FigureIF tempFigureIF = (FigureIF) temp;
+				out.printf("%s\n", tempFigureIF.source().text());
+			} else if (temp instanceof Problem) {
+				Problem tempProblem = (Problem) temp;
 
-				//prints the required block if it does not equal null
-				if(tempProblem.requiredBlock()!= null)
-					out.printf("%s\n",tempProblem.requiredBlock());
-				
-				//prints the question
-				out.printf("%s\n",tempProblem.question().text());
+				// prints the required block if it does not equal null
+				if (tempProblem.requiredBlock() != null)
+					out.printf("%s\n", tempProblem.requiredBlock());
 
-				//prints all the answers
-				for(int index=0; index<tempProblem.answers().length; index++){
-					out.printf("%s\n",tempProblem.answers()[index].source().text());
-					}//end of for loop
-			}//endo of if(temp instanceof Problem) 
-		}//endo of for loop (int i=0; i< E.elements().toArray().length; i++)
+				// prints the question
+				out.printf("%s\n", tempProblem.question().text());
+
+				// prints all the answers
+				for (int index = 0; index < tempProblem.answers().length; index++) {
+					out.printf("%s\n", tempProblem.answers()[index].source()
+							.text());
+				}// end of for loop
+			}// endo of if(temp instanceof Problem)
+		}// endo of for loop (int i=0; i< E.elements().toArray().length; i++)
 
 		// output the end block for exam
 		out.printf("%s\n", E.finalBlock());
 
-	}//end of write(PrintWriter out)
+	}// end of write(PrintWriter out)
 
-
-	//private vars
+	// private vars
 	private ExamIF E;
-}//end of class 
+}// end of class
