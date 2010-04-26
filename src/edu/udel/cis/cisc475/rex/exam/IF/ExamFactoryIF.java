63660c719971feb2018264b38d9e1e7797949e2a
martz <martz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-26 20:37:41 +0000

    ExamFactoryIF.java: Updated the JavaDoc comments to reflect the JavaDoc comments in ExamFactory.java.

--- src/edu/udel/cis/cisc475/rex/exam/IF/ExamFactoryIF.java
+++ src/edu/udel/cis/cisc475/rex/exam/IF/ExamFactoryIF.java
@@ -10,68 +10,80 @@ import edu.udel.cis.cisc475.rex.source.IF.*;
 public interface ExamFactoryIF {
 	
 	/**
+	 * Creates a new instance ExamIF for use as the master repository.
 	 * 
-	 * @return
+	 * @return new master instance of ExamIF
 	 */
     ExamIF newMasterExam();
     
-    /**
-     * 
-     * @return
-     */
+	/**
+	 * Creates a new instance of ExamIF for use as a output Exam.
+	 * 
+	 * @return new instance of ExamIF
+	 */
     ExamIF newGeneratedExam();
     
-    /**
-     * 
-     * @param topic
-     * @param label
-     * @param text
-     * @return
-     */
-    @Deprecated
-    BlockIF newBlock(String topic, String label, SourceIF text);
+	/**
+	 * Creates a new instance of BlockIF.
+	 * 
+	 * @return BlockIF
+	 * @deprecated Replaced by {@link #newBlock(String, SourceIF)}
+	 */
+	@Deprecated
+   BlockIF newBlock(String topic, String label, SourceIF text);
     
-    /**
-     * 
-     * @param label
-     * @param text
-     * @return
-     */
+	/**
+	 * Creates a new instance of BlockIF.
+	 * 
+	 * @param label String: The label given to this BlockIF
+	 * @param text SourceIF: The source from the UEF that defines this BlockIF 
+	 * @return new instance of BlockIF
+	 * @exception NullPointerException If text is null
+	 */
 	 BlockIF newBlock(String label, SourceIF text);
     
-    /**
-     * 
-     * @param label
-     * @param text
-     * @return
-     */
+		/**
+		 * Creates a new instance of FigureIF.
+		 * 
+		 * @param label String: The label given to this FigureIF
+		 * @param text SourceIF: The source from the UEF that defines this FigureIF 
+		 * @return new instance of FigureIF
+		 * @exception NullPointerException If text is null
+		 */
     FigureIF newFigure(String label, SourceIF text);
     
     /**
-     * 
-     * @param correct
-     * @param text
-     * @return
-     */
-    AnswerIF newAnswer(boolean correct, SourceIF text);
+	 * Creates a new instance of AnswerIF.
+	 * 
+	 * @param correct boolean: Determines if the AnswerIF is a correct answer
+	 * @param text SourceIF: The source from the UEF that defines this answer
+	 * @return new instance of AnswerIF
+	 * @exception NullPointerException If text is null
+	 */
+	  AnswerIF newAnswer(boolean correct, SourceIF text);
     
-    /**
-     * 
-     * @param correct
-     * @param index
-     * @param text
-     * @return
-     */
+		/**
+		 * Creates a new instance of FixedAnswerIF.
+		 * 	
+		 * @param correct boolean: Determines if the FixedAnswerIF is a correct answer
+		 * @param index int: The position that this FixedAnswerIF must be placed in.
+		 * @param text SourceIF: The source from the UEF that defines this FixedAnswerIF 
+		 * @return new instance of FixedAnswerIF
+		 * @exception NullPointerException If text is null
+		 */
     FixedAnswerIF newFixedAnswer(boolean correct, int index, SourceIF text);
     
-    /**
-     * 
-     * @param topic
-     * @param label
-     * @param question
-     * @param answers
-     * @return
-     */
-    ProblemIF newProblem(String topic, String label, SourceIF question, AnswerIF[] answers);
+	/**
+	 * Creates a new instance of ProblemIF.
+	 * 
+	 * 
+	 * @param topic String: The topic of this ProblemIF
+	 * @param label String: The label given to this ProblemIF
+	 * @param question SourceIF: The source from the UEF that defines this ProblemIF
+	 * @param answers AnswerIF[]: An array of answers for this ProblemIF 
+	 * @return new instance of ProblemIF
+	 * @exception NullPointerException If topic, question, answers, or any element in answers is null.
+	 */
+  ProblemIF newProblem(String topic, String label, SourceIF question, AnswerIF[] answers);
     
 }
