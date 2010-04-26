d69864b10cae0ff7ab2350881816e3466b5643f0
fitzpatr <fitzpatr@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-26 21:58:32 +0000

    BlockTest.java, ExamTest.java: Updated tests to work with and test block's new implementiation of making topics optional and keeping track of the topic via declareUse.

--- trunk/test-exam/edu/udel/cis/cisc475/rex/exam/BlockTest.java
+++ trunk/test-exam/edu/udel/cis/cisc475/rex/exam/BlockTest.java
@@ -8,6 +8,7 @@ package edu.udel.cis.cisc475.rex.exam;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,8 +16,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
 import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FixedAnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
 import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
 import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
@@ -35,9 +40,9 @@ public class BlockTest {
 	
 	private static SourceIF blockSource;
 	private static BlockIF block;
+	private static ProblemIF problem;
 	
 	private static String testUEFfilename = "testFileName.txt";
-	private static String testTopic = "test Topic";
 	private static String testLabel = "test Label";
 	
 	@BeforeClass
@@ -53,6 +58,8 @@ public class BlockTest {
 		// TODO Uncomment when entry point is available
 		//examFactory = Exams.newExamFactory();
 		examFactory = new ExamFactory();
+		
+		problem = createTestProblem();
 	}
 	
 	@AfterClass
@@ -83,5 +90,59 @@ public class BlockTest {
 		assertNotNull(result.text());
 		assertEquals(blockSource.text(), result.text());
 		assertEquals(blockSource, result);
+	}
+	
+	@Test
+	public void testGetTopic(){
+		ExamIF exam = examFactory.newGeneratedExam();
+		
+		exam.addElement(block);
+		exam.addElement(problem);
+		
+		assertNull(block.topic());
+		
+		exam.declareUse(problem, block);
+		assertEquals("Test Problem Topic", block.topic());
+		
+	}
+
+	private static ProblemIF createTestProblem() {
+		SourceIF	testQuestionSource;
+		SourceIF testAnswerSource;
+		AnswerIF[] testAnswers = new AnswerIF[4];
+		AnswerIF testAnswer1;
+		AnswerIF testAnswer2;
+		FixedAnswerIF testAnswer3;
+		AnswerIF testAnswer4;
+		
+		//create test question source
+		testQuestionSource = sourceFactory.newSource(testUEFfilename);
+		testQuestionSource.addText("Test Question Text?");
+		
+		//create test answers
+		testAnswerSource = sourceFactory.newSource(testUEFfilename);
+		testAnswerSource.addText("Test Answer 1");
+		testAnswer1 = examFactory.newAnswer(false, testAnswerSource);
+		
+		testAnswerSource = sourceFactory.newSource(testUEFfilename);
+		testAnswerSource.addText("Test Answer 2");
+		testAnswer2 = examFactory.newAnswer(true, testAnswerSource);
+		
+		testAnswerSource = sourceFactory.newSource(testUEFfilename);
+		testAnswerSource.addText("Test Fixed Answer 3");
+		testAnswer3 = examFactory.newFixedAnswer(false, 3, testAnswerSource);
+		
+		testAnswerSource = sourceFactory.newSource(testUEFfilename);
+		testAnswerSource.addText("Test Answer 4");
+		testAnswer4 = examFactory.newAnswer(true, testAnswerSource);
+		
+		// fill array with answers
+		testAnswers[0] = testAnswer1;
+		testAnswers[1] = testAnswer2;
+		testAnswers[2] = testAnswer3;
+		testAnswers[3] = testAnswer4;
+		
+		// create test problem
+		return examFactory.newProblem("Test Problem Topic", "Test Problem Label", testQuestionSource, testAnswers);
 	}
 }
