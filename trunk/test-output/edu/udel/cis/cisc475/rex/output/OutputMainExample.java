2b1d5903cf86c563a52cdae3a1e448f2c84c0ab2
justin <justin@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 01:18:57 +0000

    Created some tests for the exam writer.  Still have to put them in Junit.

--- trunk/test-output/edu/udel/cis/cisc475/rex/output/OutputMainExample.java
+++ trunk/test-output/edu/udel/cis/cisc475/rex/output/OutputMainExample.java
@@ -4,7 +4,13 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import edu.udel.cis.cisc475.rex.exam.IF.BlockIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
+import edu.udel.cis.cisc475.rex.exam.impl.Answer;
+import edu.udel.cis.cisc475.rex.exam.impl.Block;
+import edu.udel.cis.cisc475.rex.exam.impl.Exam;
+import edu.udel.cis.cisc475.rex.exam.impl.Figure;
+import edu.udel.cis.cisc475.rex.exam.impl.Problem;
 import edu.udel.cis.cisc475.rex.output.ExamStub;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyFactoryIF;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
@@ -13,10 +19,12 @@ import edu.udel.cis.cisc475.rex.output.IF.AnswerKeyWriterIF;
 import edu.udel.cis.cisc475.rex.output.IF.ExamWriterIF;
 import edu.udel.cis.cisc475.rex.output.IF.OutputFactoryIF;
 import edu.udel.cis.cisc475.rex.output.impl.OutputFactory;
+import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
+import edu.udel.cis.cisc475.rex.source.impl.Source;
 
 /**
  * @author cardona
- * 
+ * @author justin
  */
 
 public class OutputMainExample {
@@ -73,7 +81,86 @@ public class OutputMainExample {
 		 * now lets do some exam stuff
 		 * 
 		 */
-		ExamIF EM = new ExamStub();		
+			
+		
+		
+		ExamIF EM = new Exam(true);		
+		
+		//creates the preamble
+		Source preamble = new Source("preamble");
+		preamble.addText("\\documentclass[12pt]{article}\n"); 
+		preamble.addText("\\usepackage{graphicx}\n");
+		preamble.addText("\\usepackage[letterpaper,textheight=9.5in,left=1in,textwidth=6.5in,bottom=1in]{geometry}\n");
+		preamble.addText("\\usepackage{amsmath}\n");
+		preamble.addText("\\author{Dr T. Harvey}\n");
+		preamble.addText("\\title{test TEST}\n");
+		preamble.addText("\\date{April 08, 2010}\n");
+		preamble.addText("\\begin{document}\n");
+		preamble.addText("\\maketitle\n");
+		preamble.addText("\\newpage\n");
+	
+		//sets the preamble
+		EM.setPreamble(preamble);
+		
+		//creates the front matter
+		Source frontMatter = new Source("frontmatter.txt");
+		frontMatter.addText("\\begin{document}\n");
+		frontMatter.addText("\\maketitle\n");
+		frontMatter.addText("\\newpage\n");
+		
+		//sets the front matter
+		EM.setFrontMatter(frontMatter);
+		
+		//creates the final matter
+		Source finalSource = new Source("finalBlock.txt");
+		finalSource.addText("\\end{document}");
+		Block finalBlock = new Block("Final topic", finalSource);
+		
+		//sets the final block
+		EM.setFinalBlock(finalBlock);
+	
+		
+		
+		//creates a test block case
+		Source testSourceBlock = new Source("test source block");
+		testSourceBlock.addText("this is text inside the block");
+		Block testblock = new Block("test block", testSourceBlock);
+		// creates a test figure case
+		Source testSourceFigure = new Source("test source figure");
+		testSourceFigure.addText("this is the text inside the figure");
+		Figure testFigure = new Figure("test figure", testSourceFigure);
+		
+		//creates a test problem case
+		Source testSourceProblem = new Source("test source problem");
+		testSourceProblem.addText("this is the text inside the problem");
+		
+		Source ans1Source = new Source("ans1");
+		ans1Source.addText("this is the answer 1");
+		Answer ans1 = new Answer(true,ans1Source);
+		
+		Source ans2Source = new Source("ans2");
+		ans2Source.addText("this is the answer 2");
+		Answer ans2 = new Answer(true,ans2Source);
+		
+		Answer[] answerArray = {ans1, ans2};
+		
+		Problem testProblem = new Problem("test topic", "test label", testSourceProblem, answerArray );
+		
+		
+		
+		//creates a test problem case with a block
+		Source testSourceProblem2 = new Source("test source problem 2");
+		testSourceProblem.addText("this is the text inside the problem2");
+		
+		
+		Problem testProblem2= new Problem("test topic", "test label", testSourceProblem, answerArray );
+		testProblem2.setRequiredBlock(testblock);
+		
+		
+		EM.addElement(testblock);
+		EM.addElement(testFigure);
+		EM.addElement(testProblem);
+		EM.addElement(testProblem2);
 		ExamWriterIF EW = OF.newExamWriter (EM);		
 		
 		EW.write(pw);
