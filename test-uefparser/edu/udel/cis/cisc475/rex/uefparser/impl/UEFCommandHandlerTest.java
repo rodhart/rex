507799eed2767aa7abc7aa68b53caeba0dfd5467
alandweh <alandweh@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 02:52:26 +0000

    Added junit class for testing UEFCommandHandler. Currently contains tests for processAnswer(). Added corresponding processAnswerTestFile.tex to the examples directory.

---
+++ test-uefparser/edu/udel/cis/cisc475/rex/uefparser/impl/UEFCommandHandlerTest.java
@@ -0,0 +1,84 @@
+package edu.udel.cis.cisc475.rex.uefparser.impl;
+
+import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.FixedAnswerIF;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.io.File;
+import org.junit.Test;
+
+/**
+ *
+ * @author Aaron Myles Landwehr
+ *
+ */
+public class UEFCommandHandlerTest
+{
+
+	/**
+	 * Tests different argument combinations for the parseForArgument() method. The file read
+	 * in attempts to identify different scenerios that could cause parsing failure.
+	 *
+	 * @throws Exception If the test somehow throws some kind of exception.
+	 * The likely scenerio is that the needed resource file isn't being found for some reason.
+	 */
+	@Test
+	public void processAnswerTest() throws Exception
+	{
+		UEFParser parser = new UEFParser();
+
+		//Open the file to parse.
+		File file = new File("." + File.separator + "examples" + File.separator + "processAnswerTestFile.tex");
+		parser.parseForAllCommands(file);
+
+		//get a reference to the handler to allow direct calls to the process methods of the handler
+		UEFCommandHandler handler = parser.getUEFCommandHandler();
+
+		AnswerIF answer = handler.processAnswer(0);
+		assertEquals(true, answer.isCorrect());
+		assertEquals("processAnswerTestFile.tex", answer.source().filename());
+		assertEquals(1, answer.source().startLine());
+		assertEquals(1, answer.source().lastLine());
+		assertEquals(1, answer.source().startColumn());
+		assertEquals(26, answer.source().lastColumn());
+		assertEquals("\\answer[correct] $2^{-1}$", answer.source().text());
+
+		answer = handler.processAnswer(1);
+		assertEquals(true, answer.isCorrect());
+		assertEquals("processAnswerTestFile.tex", answer.source().filename());
+		assertEquals(2, answer.source().startLine());
+		assertEquals(2, answer.source().lastLine());
+		assertEquals(1, answer.source().startColumn());
+		assertEquals(23, answer.source().lastColumn());
+		assertEquals("\\answer[correct] $2^0$", answer.source().text());
+
+		answer = handler.processAnswer(2);
+		assertEquals(false, answer.isCorrect());
+		assertEquals("processAnswerTestFile.tex", answer.source().filename());
+		assertEquals(2, answer.source().startLine());
+		assertEquals(2, answer.source().lastLine());
+		assertEquals(23, answer.source().startColumn());
+		assertEquals(36, answer.source().lastColumn());
+		assertEquals("\\answer $2^1$", answer.source().text());
+
+		answer = handler.processAnswer(3);
+		assertEquals(false, answer.isCorrect());
+		assertEquals("processAnswerTestFile.tex", answer.source().filename());
+		assertEquals(3, answer.source().startLine());
+		assertEquals(4, answer.source().lastLine());
+		assertEquals(8, answer.source().startColumn());
+		assertEquals(26, answer.source().lastColumn());
+		assertEquals("\\answer $3^1$\n       ends on this line.", answer.source().text());
+
+		answer = handler.processAnswer(4);
+		assertTrue(answer instanceof FixedAnswerIF);
+		assertEquals(false, answer.isCorrect());
+		assertEquals(4, ((FixedAnswerIF) answer).index());
+		assertEquals("processAnswerTestFile.tex", answer.source().filename());
+		assertEquals(5, answer.source().startLine());
+		assertEquals(5, answer.source().lastLine());
+		assertEquals(1, answer.source().startColumn());
+		assertEquals(33, answer.source().lastColumn());
+		assertEquals("\\answer[fixed] none of the above", answer.source().text());
+	}
+}
