b04fc086505f3b6aa69f7c5cba3f810360c457c2
wgordon <wgordon@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 15:19:47 +0000

    Created a sample config and exam, doesnt successfully run completely through generate()

--- trunk/test-generate/edu/udel/cis/cisc475/rex/generate/GeneratorTest.java
+++ trunk/test-generate/edu/udel/cis/cisc475/rex/generate/GeneratorTest.java
@@ -26,6 +26,7 @@ import edu.udel.cis.cisc475.rex.exam.IF.ExamFactoryIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.IF.ProblemIF;
 import edu.udel.cis.cisc475.rex.exam.generatestubs.ExamFactoryStub;
+import edu.udel.cis.cisc475.rex.exam.impl.Answer;
 import edu.udel.cis.cisc475.rex.exam.impl.ExamFactory;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorFactoryIF;
 import edu.udel.cis.cisc475.rex.generate.IF.GeneratorIF;
@@ -33,6 +34,7 @@ import edu.udel.cis.cisc475.rex.generate.impl.GeneratorFactory;
 import edu.udel.cis.cisc475.rex.interval.IF.IntervalFactoryIF;
 import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
 import edu.udel.cis.cisc475.rex.interval.generatestubs.IntervalFactoryStub;
+import edu.udel.cis.cisc475.rex.interval.impl.IntervalFactory;
 import edu.udel.cis.cisc475.rex.key.Keys;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyFactoryIF;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
@@ -40,10 +42,11 @@ import edu.udel.cis.cisc475.rex.key.generatestubs.AnswerKeyFactoryStub;
 import edu.udel.cis.cisc475.rex.source.IF.SourceFactoryIF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
 import edu.udel.cis.cisc475.rex.source.generatestubs.SourceFactoryStub;
+import edu.udel.cis.cisc475.rex.source.impl.SourceFactory;
 
 public class GeneratorTest {
 
-	public final static boolean useStubs = true;
+	public final static boolean useStubs = false;
 	
 	public int numexams;
 	
@@ -89,30 +92,50 @@ public class GeneratorTest {
 			sourceFactory = new SourceFactoryStub();
 		} else {
 			configFactory = Configs.newConfigFactory();
+			masterFactory = new ExamFactory();
 			answerKeyFactory = Keys.newAnswerKeyFactory();
+			intervalFactory = new IntervalFactory(); //Intervals not made yet
+			sourceFactory = new SourceFactory(); //Sources not made
 		}
+		
 		generatorFactory = new GeneratorFactory();
 		
-		master1 = masterFactory.newMasterExam();
-		master1.problems();
 		
+		// Create sample master exam
+		master1 = masterFactory.newMasterExam();
+		
+		SourceIF question1 = sourceFactory.newSource("testFile.txt");		
+		question1.addText("question1");		
+		AnswerIF[] answers1 = new Answer[2];
+		answers1[0] = new Answer(true, sourceFactory.newSource("option1"));
+		answers1[1] = new Answer(false, sourceFactory.newSource("option2"));
+		ProblemIF prob1 = masterFactory.newProblem("topic1", "label1", question1, answers1);
+		
+		SourceIF question2 = sourceFactory.newSource("testFile.txt");
+		question2.addText("question2");		
+		AnswerIF[] answers2 = new Answer[2];
+		answers2[0] = new Answer(false, sourceFactory.newSource("option3"));
+		answers2[1] = new Answer(true, sourceFactory.newSource("option4"));
+		ProblemIF prob2 = masterFactory.newProblem("topic2", "label2", question2, answers2);
+		
+		master1.addElement(prob1);
+		master1.addElement(prob2);
+		
+		
+		// Create sample Config
 		config1 = configFactory.newConfig(true, 1);
 		
-		Collection<ConstraintIF> theConstraints = config1.constraints();
-		ArrayList<GroupConstraintIF> groupConstraints = new ArrayList<GroupConstraintIF>();
-		ArrayList<RequiredProblemConstraintIF> reqProbConstraints = new ArrayList<RequiredProblemConstraintIF>();
+		SourceIF source1 = sourceFactory.newSource("testFile.txt");
+		source1.addText("question1");
+		SourceIF source2 = sourceFactory.newSource("testFile.txt");
+		source2.addText("question2");
+		IntervalIF interval = intervalFactory.interval(true, 1.0, true, 5.0);
+		config1.addRequiredProblemConstraint("label1", 4, source1);
+		config1.addGroupConstraint("topic2", interval, 1, 3, source2);
+		String[] versions = {"Version1"};
+		config1.setVersionStrings(versions);
 		
-		for (ConstraintIF c : theConstraints)
-		{				
-			if (c.getClass().isInstance(GroupConstraintIF.class))	
-			{
-				groupConstraints.add((GroupConstraintIF) c);
-			}
-			else if (c.getClass().isInstance(RequiredProblemConstraintIF.class))
-			{
-				reqProbConstraints.add((RequiredProblemConstraintIF) c);
-			}
-		}
+		generator1 = generatorFactory.newGenerator(master1, config1);
 		
 		/* Dan's Work
 		interval1 = intervalFactory.interval(false, 10.0, true, 25.0);
