8b09ed930b0ca768a93ad18886af2bb575bdec06
bouchard <bouchard@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 17:03:37 +0000

    Added Javadoc, updated comments, cleaned up code. Functionality not changed from previous revision of code.

--- trunk/src/edu/udel/cis/cisc475/rex/interval/IF/IntervalFactoryIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/interval/IF/IntervalFactoryIF.java
@@ -2,13 +2,15 @@ package edu.udel.cis.cisc475.rex.interval.IF;
 
 import edu.udel.cis.cisc475.rex.err.RexUnsatisfiableException;
 
-/*
+/**
+ * 
+ * Interface for IntervalFactory.
+ * 
  * @author Kyle Bouchard (bouchard)
  *
  */
-
 public interface IntervalFactoryIF {
 	IntervalIF interval(boolean strictLow, Double low, boolean strictHigh,
 			Double high) throws RexUnsatisfiableException;
-}// end of interface
+}
 
--- trunk/src/edu/udel/cis/cisc475/rex/interval/IF/IntervalIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/interval/IF/IntervalIF.java
@@ -1,18 +1,17 @@
 package edu.udel.cis.cisc475.rex.interval.IF;
 
-/*
- * @author team 2
+/**
+ * Interface for Interval
+ * 
+ * @author Kyle Bouchard (bouchard)
  *
  */
-
 public interface IntervalIF {
+
 	Double low();
-
 	boolean strictLow();
-
 	Double high();
-
 	boolean strictHigh();
 
 	boolean contains(double value);
-}// end of interface
+}
