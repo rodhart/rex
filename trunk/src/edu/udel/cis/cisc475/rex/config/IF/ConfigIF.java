4db9c757e1c98a5d03eb6c4f32ae473a31154ce2
aplatt <aplatt@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 17:33:13 +0000

    javadoc corrections

--- trunk/src/edu/udel/cis/cisc475/rex/config/IF/ConfigIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/config/IF/ConfigIF.java
@@ -6,36 +6,87 @@ import edu.udel.cis.cisc475.rex.interval.IF.IntervalIF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
 
 /**
- * @author Anthony Platt (aplatt)
- * @author Jeremy Verchick (verchick)
- *
  * ConfigIF is the interface for the Config class. 
  * It provides a list of the class's public methods.
  * 
+ * @author Anthony Platt (aplatt)
+ * @author Jeremy Verchick (verchick)
+ *
  */
 
 public interface ConfigIF {
+	/**
+	 * Returns true or false whether or not a pdf should be generated
+	 */
 	boolean pdfOption();
 
+	/**
+	 * Returns exam seed
+	 */
 	long seed();
 
+	/**
+	 * Returns the Group and Problem Constraints that have been stored in the 
+	 * constraints collection.
+	 */
 	Collection<ConstraintIF> constraints();
 
+	/**
+	 * Returns the final block
+	 */
 	String finalBlock();
 
+	/**
+	 * Returns the version strings for exam version differentiation
+	 */
 	String[] versionStrings();
 
+	/**
+	 * Returns the number of versions to be generated
+	 */
 	int numVersions();
 
+	/**
+	 * Sets the exam seed number
+	 * @param value - the exam seed value
+	 */
 	void setSeed(long value);
 
+	/**
+	 * sets the version strings for exam version differentiation
+	 * @param names - the list of version strings.
+	 */
 	void setVersionStrings(String[] names);
 
+	/**
+	 * Sets the final block
+	 * @param label  - the label of the final block
+	 */
 	void setFinalBlock(String label);
 
+	/**
+	 * Constructor creates a new GroupConstraint object with the passed parameters and adds it to
+	 * the constraints collection.  
+	 * 
+	 * @param topic - the required topic
+	 * @param difficulty - the range of problem difficulties to be included in the topic
+	 * @param numProblems - the number of problems to include within the topic
+	 * @param points - the point value of the problem
+	 * @param source - the location in the ECF file of the group constraint
+	 * 
+	 */
 	GroupConstraintIF addGroupConstraint(String topic, IntervalIF difficulty,
 			int numProblems, int points, SourceIF source);
 
+	/**
+	 * Creates a new RequiredProblemConstraint object with the passed parameters and adds 
+	 * it to the constraints collection.  
+	 * 
+	 * @param label - the required problem's label
+	 * @param points - the point value of the problem
+	 * @param source - the location in the ECF file of the problem constraint
+	 * 
+	 */
 	RequiredProblemConstraintIF addRequiredProblemConstraint(String label,
 			int points, SourceIF source);
 
--- trunk/src/edu/udel/cis/cisc475/rex/config/IF/ConstraintIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/config/IF/ConstraintIF.java
@@ -3,11 +3,11 @@ package edu.udel.cis.cisc475.rex.config.IF;
 import edu.udel.cis.cisc475.rex.source.IF.SourceIF;
 
 /**
- * @author Anthony Platt (aplatt)
- * @author Jeremy Verchick (verchick)
- *
  * ConstraintIF is the interface for the Constraint class. 
  * It provides a list of the class's public methods.
+ * 
+ * @author Anthony Platt (aplatt)
+ * @author Jeremy Verchick (verchick)
  * 
  */
 
