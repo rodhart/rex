41ef07f624838ed9ff792e7bf59483ec8205c41b
schultz <schultz@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-27 02:06:58 +0000

    Fixed static void main to be public so that Rex runs

--- trunk/src/edu/udel/cis/cisc475/rex/main/Rex.java
+++ trunk/src/edu/udel/cis/cisc475/rex/main/Rex.java
@@ -25,8 +25,7 @@ import edu.udel.cis.cisc475.rex.uefparser.impl.UEFParserFactory;
 
 public class Rex {
 
-	static void main(String[] args)
-	{ 
+	public static void main(String[] args) {
 
 		int numArgs = args.length;
 		long seed = 1;
@@ -45,32 +44,32 @@ public class Rex {
 		/*
 		 * Parse command line
 		 */
-		while ( i < numArgs - 2) {
+		while (i < numArgs - 2) {
 			/*
 			 * Test for -n option and set numExams
 			 */
 			if (numExams == 1 && args[i].equals("-n")) {
-				numExams = Integer.parseInt(args[i+1]);
+				numExams = Integer.parseInt(args[i + 1]);
 				i += 2;
-			} 
+			}
 			/*
 			 * Test for -seed option and set seed
 			 */
 			else if (seed == 1 && args[i].equals("-seed")) {
-				seed = Long.parseLong(args[i+1]);
-				i+=2;
-			} 
+				seed = Long.parseLong(args[i + 1]);
+				i += 2;
+			}
 			/*
 			 * Test for -pdf option and set boolean pdfOpt
 			 */
 			else if (!pdfOpt && args[i].equals("-pdf")) {
 				pdfOpt = true;
-				i++;	
+				i++;
 			}
 			/*
 			 * Exit if an incorrect option is given
 			 */
-			else{
+			else {
 				System.err.println(args[i] + " is not a valid option!");
 				printUsage();
 				System.exit(-1);
@@ -79,136 +78,124 @@ public class Rex {
 		/*
 		 * Make sure the UEF input file is a .tex file
 		 */
-		if(args[numArgs-2].contains(".tex")){
-			uef = new File(args[numArgs-2]);
-		}
-		else{
-			System.err.println(args[numArgs-2] + " does not appear to" +
-			"be a valid .tex file!");
+		if (args[numArgs - 2].contains(".tex")) {
+			uef = new File(args[numArgs - 2]);
+		} else {
+			System.err.println(args[numArgs - 2] + " does not appear to"
+					+ "be a valid .tex file!");
 			printUsage();
 			System.exit(-1);
 		}
 		/*
 		 * Make sure the ECF input file is a .ecf file
 		 */
-		if(args[numArgs-1].contains(".ecf")){
-			ecf = new File(args[numArgs-1]);
-		}
-		else{
-			System.err.println(args[numArgs-2] + " does not appear to" +
-			"be a valid .ecf file!");
+		if (args[numArgs - 1].contains(".ecf")) {
+			ecf = new File(args[numArgs - 1]);
+		} else {
+			System.err.println(args[numArgs - 2] + " does not appear to"
+					+ "be a valid .ecf file!");
 			printUsage();
 			System.exit(-1);
 		}
 		/*
-		 * Create an EcfParserFactory
-		 * Get a new parser
-		 * Parse the ECF to get theConfig
+		 * Create an EcfParserFactory Get a new parser Parse the ECF to get
+		 * theConfig
 		 */
 		EcfParserFactoryIF theEcfParserFactory = new EcfParserFactory();
 		EcfParserIF theEcfParser = theEcfParserFactory.newParser(numExams);
 		ConfigIF theConfig = theEcfParser.parse(ecf);
 		/*
-		 * Create an UefParserFactory
-		 * Get a new parser
-		 * Parse the UEF to get theMaster
+		 * Create an UefParserFactory Get a new parser Parse the UEF to get
+		 * theMaster
 		 */
-		UEFParserFactoryIF theUefParserFactory = new UEFParserFactory(); 
+		UEFParserFactoryIF theUefParserFactory = new UEFParserFactory();
 		UEFParserIF theUefParser = theUefParserFactory.newUEFParser();
 		ExamIF theMaster = theUefParser.parse(uef);
 		/*
-		 * Create the GeneratorFactory
-		 * Create the Generator
+		 * Create the GeneratorFactory Create the Generator
 		 */
 		GeneratorFactoryIF theGeneratorFactory = new GeneratorFactory();
-		
 
-		GeneratorIF theGenerator = theGeneratorFactory.newGenerator(theMaster, theConfig);
+		GeneratorIF theGenerator = theGeneratorFactory.newGenerator(theMaster,
+				theConfig);
 
-			//TODO Write an appropriate error.
-		
+		// TODO Write an appropriate error.
+
 		/*
-		 * Create the container for the generated exams
-		 * and answer keys
+		 * Create the container for the generated exams and answer keys
 		 */
-		ExamIF [] theExams = new ExamIF[numExams];
-		AnswerKeyIF [] theAnswerKeys = new AnswerKeyIF[numExams];
+		ExamIF[] theExams = new ExamIF[numExams];
+		AnswerKeyIF[] theAnswerKeys = new AnswerKeyIF[numExams];
 
 		/*
 		 
 		 */
-		for(i = 0; i < numExams; i++){
-			
+		for (i = 0; i < numExams; i++) {
+
 		}
 		/*
 		 * Set up output files and file streams
 		 */
-		File [] theLatexFiles = new File[numExams];
-		File [] theKeyFiles = new File[numExams];
-		PrintWriter [] theLatexWriters = new PrintWriter[numExams];
-		PrintWriter [] theKeyWriters = new PrintWriter[numExams];
+		File[] theLatexFiles = new File[numExams];
+		File[] theKeyFiles = new File[numExams];
+		PrintWriter[] theLatexWriters = new PrintWriter[numExams];
+		PrintWriter[] theKeyWriters = new PrintWriter[numExams];
 		/*
 		 * Set up the output module containers
 		 */
 		OutputFactoryIF theOutputFactory = new OutputFactory();
-		ExamWriterIF [] theExamWriters = new ExamWriterIF[numExams];
-		AnswerKeyWriterIF [] theAnswerKeyWriters = new AnswerKeyWriterIF[numExams];
+		ExamWriterIF[] theExamWriters = new ExamWriterIF[numExams];
+		AnswerKeyWriterIF[] theAnswerKeyWriters = new AnswerKeyWriterIF[numExams];
 
 		/*
-		 * Get an exam and key
-		 * Create the actual files
-		 * Initiate the PrintWriters
+		 * Get an exam and key Create the actual files Initiate the PrintWriters
 		 * Use the output module to fill the files
 		 */
-		for (i = 0; i < numExams; i++){
+		for (i = 0; i < numExams; i++) {
 			/*
 			 * Fill the containers with the generated material
 			 */
-			
-				theExams[i] = theGenerator.getGeneratedExam(i);
-				theAnswerKeys[i] = theGenerator.getAnswerKey(i);
-			
-			
-				//TODO Write appropriate error message
-			
-			
+
+			theExams[i] = theGenerator.getGeneratedExam(i);
+			theAnswerKeys[i] = theGenerator.getAnswerKey(i);
+
+			// TODO Write appropriate error message
+
 			/*
 			 * create the File
 			 */
-			theLatexFiles[i] = new File("exam"+(i+1)+".tex");
-			theKeyFiles[i] = new File("key"+(i+1)+".txt");
-			try{
+			theLatexFiles[i] = new File("exam" + (i + 1) + ".tex");
+			theKeyFiles[i] = new File("key" + (i + 1) + ".txt");
+			try {
 				/*
 				 * create the exam(i).tex file on the file system
 				 */
 				theLatexFiles[i].createNewFile();
 
-			}
-			catch (Exception e){
-				System.err.println("Error creating "+ 
-						theLatexFiles[i].getAbsolutePath());
+			} catch (Exception e) {
+				System.err.println("Error creating "
+						+ theLatexFiles[i].getAbsolutePath());
 				e.printStackTrace();
 			}
 			/*
 			 * create the key(i).txt file on the file system
 			 */
-			try{
+			try {
 				theKeyFiles[i].createNewFile();
 
-			}
-			catch (Exception e){
-				System.err.println("Error creating "+ 
-						theKeyFiles[i].getAbsolutePath());
+			} catch (Exception e) {
+				System.err.println("Error creating "
+						+ theKeyFiles[i].getAbsolutePath());
 				e.printStackTrace();
 			}
 			/*
 			 * Create the PrintWriters from the newly created files
 			 */
 			try {
-				theLatexWriters[i] = new PrintWriter(
-						new FileOutputStream(theLatexFiles[i]));
-				theKeyWriters[i] = new PrintWriter(
-						new FileOutputStream(theKeyFiles[i]));
+				theLatexWriters[i] = new PrintWriter(new FileOutputStream(
+						theLatexFiles[i]));
+				theKeyWriters[i] = new PrintWriter(new FileOutputStream(
+						theKeyFiles[i]));
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
@@ -217,14 +204,12 @@ public class Rex {
 			 * Set up the exam writers and answer key writers
 			 */
 
-			theExamWriters[i] = 
-				theOutputFactory.newExamWriter(theExams[i]);
-			theAnswerKeyWriters[i] = 
-				theOutputFactory.newAnswerKeyWriter(theAnswerKeys[i]);
+			theExamWriters[i] = theOutputFactory.newExamWriter(theExams[i]);
+			theAnswerKeyWriters[i] = theOutputFactory
+					.newAnswerKeyWriter(theAnswerKeys[i]);
 
 			/*
-			 * Write the Exams and answer keys
-			 * to their proper files
+			 * Write the Exams and answer keys to their proper files
 			 */
 
 			theExamWriters[i].write(theLatexWriters[i]);
@@ -238,27 +223,29 @@ public class Rex {
 
 		printCompletionMessage(pdfOpt);
 	}
-	private static void printUsage(){
+
+	private static void printUsage() {
 
 		System.err
-		.println("Usage: rex [options] <UEF filename> <ECF filename>");
+				.println("Usage: rex [options] <UEF filename> <ECF filename>");
 		System.err.println("Options: ");
-		System.err.println("    -n numExams : int numExams declares"+
-		" number of exams");
-		System.err.println("    -seed seed : long seed declares"+ 
-		"the seed for randomization");
-		System.err.println("    -pdf       : sets the pdf option"+ 
-				"in order to have the exams output in pdf as well"+ 
-		" as .tex files.");
+		System.err.println("    -n numExams : int numExams declares"
+				+ " number of exams");
+		System.err.println("    -seed seed : long seed declares"
+				+ "the seed for randomization");
+		System.err.println("    -pdf       : sets the pdf option"
+				+ "in order to have the exams output in pdf as well"
+				+ " as .tex files.");
 	}
-	private static void printCompletionMessage(boolean pdf){
+
+	private static void printCompletionMessage(boolean pdf) {
 		System.out.println("Rex has completed your request!");
-		System.out.println("The exam(n).tex files hold your exams"+
-				" in latex format.");
-		System.out.println("The key(n).txt files hold your answer keys"+
-				" in plain text format.");
-		if(pdf)
-			System.out.println("The exam(n).pdf files hold your exams"+
-			" in PDF format.");
+		System.out.println("The exam(n).tex files hold your exams"
+				+ " in latex format.");
+		System.out.println("The key(n).txt files hold your answer keys"
+				+ " in plain text format.");
+		if (pdf)
+			System.out.println("The exam(n).pdf files hold your exams"
+					+ " in PDF format.");
 	}
 }
