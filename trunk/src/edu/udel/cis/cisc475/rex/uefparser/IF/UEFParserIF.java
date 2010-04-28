77676ff2c82e6eb1071709d30b3609fc5e075cda
alandweh <alandweh@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 03:02:21 +0000

    Made some UEFParserIF comments better.

--- trunk/src/edu/udel/cis/cisc475/rex/uefparser/IF/UEFParserIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/uefparser/IF/UEFParserIF.java
@@ -5,14 +5,20 @@ import java.io.File;
 import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 
 /**
- * Interface to the Universal Exam Parser (UEF).
+ * Interface for the parser of the Universal Exam File (UEF).
+ * Provides interfaces for methods that any class implementing
+ * a UEF parser must implement.
  * 
  * @author Ahmed El-Hassany
+ * @author Aaron Myles Landwehr
  * 
  */
 public interface UEFParserIF {
 	/**
-	 * Parse UEF file and generate ExamIF of it.
+	 * Any class implementing this method needs to be able
+	 * to internally open and parse any tex file using the
+	 * exam documentclass. It also must be able to provide
+	 * a valid and correct ExamIF object from that.
 	 * 
 	 * @param file
 	 *            the file handler for the ueffile.
