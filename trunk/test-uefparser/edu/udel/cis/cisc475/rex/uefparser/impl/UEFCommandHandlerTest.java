9d48d5a6f08ad0e59e4077610381414ace41e8c5
alandweh <alandweh@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 00:21:10 +0000

    Added an initial testing for the return of a valid ExamIF from the UEFCommandHandler.process().

--- trunk/test-uefparser/edu/udel/cis/cisc475/rex/uefparser/impl/UEFCommandHandlerTest.java
+++ trunk/test-uefparser/edu/udel/cis/cisc475/rex/uefparser/impl/UEFCommandHandlerTest.java
@@ -1,10 +1,12 @@
 package edu.udel.cis.cisc475.rex.uefparser.impl;
 
 import edu.udel.cis.cisc475.rex.exam.IF.AnswerIF;
+import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.exam.IF.FixedAnswerIF;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.io.File;
+import java.util.Collection;
 import org.junit.Test;
 
 /**
@@ -16,8 +18,8 @@ public class UEFCommandHandlerTest
 {
 
 	/**
-	 * Tests different argument combinations for the parseForArgument() method. The file read
-	 * in attempts to identify different scenerios that could cause parsing failure.
+	 * Tests that processAnswer() returns correct AnswerIF objects after reading in a number of
+	 * \answer commands from a file.
 	 *
 	 * @throws Exception If the test somehow throws some kind of exception.
 	 * The likely scenerio is that the needed resource file isn't being found for some reason.
@@ -80,5 +82,33 @@ public class UEFCommandHandlerTest
 		assertEquals(1, answer.source().startColumn());
 		assertEquals(33, answer.source().lastColumn());
 		assertEquals("\\answer[fixed] none of the above", answer.source().text());
+	}
+
+	/**
+	 * Tests that process() returns a correct ExamIF after reading in a valid UEF file.
+	 *
+	 * @throws Exception If the test somehow throws some kind of exception.
+	 * The likely scenerio is that the needed resource file isn't being found for some reason.
+	 */
+	@Test
+	public void process() throws Exception
+	{
+		UEFParser parser = new UEFParser();
+
+		//Open the file to parse.
+		File file = new File("." + File.separator + "examples" + File.separator + "exam.tex");
+		parser.parseForAllCommands(file);
+
+		//get a reference to the handler to allow direct calls to the process methods of the handler
+		UEFCommandHandler handler = parser.getUEFCommandHandler();
+
+		ExamIF exam = handler.process();
+
+		Collection<String> topics = exam.topics();
+
+		assertTrue(topics.contains("logical"));
+		assertTrue(topics.contains("example topic"));
+		assertTrue(topics.contains("matrix matlab"));
+		assertTrue(topics.contains("function call"));
 	}
 }
