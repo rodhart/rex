2df5ae1567fef7c8e994df6af558d77aa2f66cee
cardona <cardona@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 22:21:18 +0000

    added chooseT shell

--- trunk/src/edu/udel/cis/cisc475/rex/random/IF/RandomizerIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/random/IF/RandomizerIF.java
@@ -15,9 +15,19 @@ import edu.udel.cis.cisc475.rex.err.RexException;
  *
  */
 public interface RandomizerIF {
-
-
-
+	/**
+	 *  General purpose random chooser: 
+	 * can be used to permute answers 
+	 * (let numItems=items.length) 
+	 * permute topics, select problems 
+	 * satisfying constraints (first filter then select) 
+	 * 
+	 * @param numItems number of items to return
+	 * @param items the arrays of objects to choose from
+	 * @return an array of chosen objects
+	 * @throws Exception 
+	 */
+	Object[] choose(int numItems, Object[] items);
 
 	/**
 	 *  General purpose random chooser: 
@@ -27,10 +37,9 @@ public interface RandomizerIF {
 	 * satisfying constraints (first filter then select) 
 	 * 
 	 * @param numItems number of items to return
-	 * @param items the arrays of objects o choose from
-	 * @return an array of chosen objects
+	 * @param items the arrays of type T to choose from
+	 * @return an array of type T
 	 * @throws Exception 
 	 */
-	Object[] choose(int numItems, Object[] items);
-
+	<T> T[] chooseT(int numItems, T[] items);
 }// end of interface
--- trunk/src/edu/udel/cis/cisc475/rex/random/impl/Randomizer.java
+++ trunk/src/edu/udel/cis/cisc475/rex/random/impl/Randomizer.java
@@ -74,6 +74,8 @@ public class Randomizer implements RandomizerIF {
 			throw new IllegalArgumentException("Number of items: " + numItems + "\n" + 
 					"Number of of items in the array: " + (items.length + 1));
 		}
+
+		
 		
 		while(shuffledItems.size()!=numItems ){
 			//gets a random number in the array's range
@@ -88,7 +90,62 @@ public class Randomizer implements RandomizerIF {
 		
 		//returns the shuffled list as an array
 		return shuffledItems.toArray();
+	}//end of public Object[] choose(int numItems, Object[] items) {
+
+
+	
+	
+	/**
+	 *  General purpose random chooser: can be used to permute answers 
+	 * (let numItems=items.length) permute topics, select problems 
+	 * satisfying constraints (first filter then select) 
+	 * 
+	 * @param numItems  number of items to return
+	 * @param items  the type T array to choose from
+	 * @return an array of chosen objects
+	 */
+public	<T> T[] chooseT(int numItems, T[] items){	
+/*
+	int index;
+	//holds the original list
+//	ArrayList itemsArray = new ArrayList();
+	//holds the shuffled list
+	ArrayList shuffledItems  = new ArrayList();
+
+	//adds all the items to the arraylist
+	for(int i=0; i< items.length; i++){
+		itemsArray.add(items[i]);	
 	}
 
+	//if picked items is less then zero return null
+	if(numItems < 0){
+		// Should be changed to RexException when it's implemented
+		//throw new Exception("Number of items: " + numItems + "\n numItems <= 0");
+		return null;
+	}
+	//if they request more objects then in the list
+	if(numItems > items.length + 1){	
+		//throw new Exception("Number of items: " + numItems + "\n"
+			//	+ "Length of array: " + (items.length+1));
+		return null;
+	}
+	
+	while(shuffledItems.size()!=numItems ){
+		//gets a random number in the array's range
+		index = randnum.nextInt(itemsArray.size());
+		//adds that item to the shuffled list
+		shuffledItems.add(itemsArray.get(index));
+		//removes that item from the items array
+		itemsArray.remove(index);
+	}
+
+	index = 0;
+	
+	//returns the shuffled list as an array
+	*/
+//	return shuffledItems.toArray();
+	return items;	
+	}//end of public Object[] choose(int numItems, Object[] items) {
+	
 }//end of class 
 
