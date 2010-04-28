c0bbcddd88ab186e25e693fc6876d23e26cc1424
zhine <zhine@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 01:03:48 +0000

    Created constructors for each exception that allows them to take a String (the class that throws the exception can pass a string with specific information gathered from a SourceIF in it). Also edited the GroupConstraintContainer to pass the error message rather than print it out from within the class itself.

--- src/edu/udel/cis/cisc475/rex/err/RexException.java
+++ src/edu/udel/cis/cisc475/rex/err/RexException.java
@@ -1,12 +1,20 @@
 package edu.udel.cis.cisc475.rex.err;
 
 public class RexException extends Exception {
-
+	
 	/**
-	 * @author Team 3
+	 * @author Zach Hine
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	
+	public RexException() {
+		super("The operation you have requested cannot be completed.");
+	}
+
+	public RexException(String message) {
+		super(message);
+	}
 	
 
 }
--- src/edu/udel/cis/cisc475/rex/err/RexParseException.java
+++ src/edu/udel/cis/cisc475/rex/err/RexParseException.java
@@ -3,10 +3,19 @@ package edu.udel.cis.cisc475.rex.err;
 public class RexParseException extends RexException {
 
 	/**
-	 * @author Team 3
+	 * @author Zach Hine
 	 */
+	
 	private static final long serialVersionUID = 1L;
 	
+	public RexParseException()
+	{
+		super("The parsing operation you have attempted could not be completed.");
+	}
 	
-
+	public RexParseException(String message)
+	{
+		super(message);
+	}
+	
 }
