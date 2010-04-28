ff933554c937b51a2061c1d9c3284a7bac28c26a
kiernan <kiernan@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 00:45:53 +0000

    fixed javadocs

--- trunk/src/edu/udel/cis/cisc475/rex/output/IF/OutputFactoryIF.java
+++ trunk/src/edu/udel/cis/cisc475/rex/output/IF/OutputFactoryIF.java
@@ -4,8 +4,7 @@ import edu.udel.cis.cisc475.rex.exam.IF.ExamIF;
 import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
 
 /**
- * The OutputFactoryIF specifies classes for writing
- * exam objects and corresponding answerKey objects
+ * Factory class for creating objects of type ExamWriterIF and AnswerKeyIF
  * 
  * @author cardona
  */
@@ -13,15 +12,14 @@ import edu.udel.cis.cisc475.rex.key.IF.AnswerKeyIF;
 public interface OutputFactoryIF {
 	
 	/**
-	 * This method utilizes a print writer and an precreated Exam
-     * class to output parts of the exam class to the print writer.
+	 * Create new object that implements ExamWriterIF
      * 
 	 * @param exam an exam object with preamble, front matter, and problems
 	 */
 	ExamWriterIF newExamWriter(ExamIF exam); 
 	
 	/**
-	 * This method writes an answerKey object to any PrintWriter object
+	 * Create new object that implements AnswerKeyWriterIF
 	 *  
 	 * @param answerKey an answerKey object that corresponds to a completed exam object
 	 */
