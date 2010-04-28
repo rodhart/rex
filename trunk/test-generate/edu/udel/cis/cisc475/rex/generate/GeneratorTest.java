6980790057c64f0c759f803e2c004f807273a68d
wgordon <wgordon@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-28 06:22:54 +0000

    Removed unnecessary library

--- trunk/test-generate/edu/udel/cis/cisc475/rex/generate/GeneratorTest.java
+++ trunk/test-generate/edu/udel/cis/cisc475/rex/generate/GeneratorTest.java
@@ -1,7 +1,9 @@
 package edu.udel.cis.cisc475.rex.generate;
 
-import static org.junit.Assert.*;
+
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
