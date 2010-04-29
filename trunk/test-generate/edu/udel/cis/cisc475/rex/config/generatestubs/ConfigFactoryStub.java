87c2cd827cc269a5a7310b5fcd8e498ad8396ee8
aplatt <aplatt@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-04-29 01:16:55 +0000

    modified to reflect changes to configFactory

--- trunk/test-generate/edu/udel/cis/cisc475/rex/config/generatestubs/ConfigFactoryStub.java
+++ trunk/test-generate/edu/udel/cis/cisc475/rex/config/generatestubs/ConfigFactoryStub.java
@@ -18,21 +18,4 @@ public class ConfigFactoryStub implements ConfigFactoryIF {
 		return new ConfigStub(pdf, numVersions);
 	}
 
-	@Override
-	public ConstraintIF newConstraint(SourceIF source) {
-		return new Constraint(source);
-	}
-
-	@Override
-	public GroupConstraintIF newGroupConstraint(IntervalIF difficulty,
-			int numProblems, int points, String topic, SourceIF source) {
-		return new GroupConstraint(difficulty, numProblems, points, topic, source);
-	}
-
-	@Override
-	public RequiredProblemConstraintIF newRequiredProblemConstraint(
-			String label, int points, SourceIF source) {
-		return new RequiredProblemConstraint(label, points, source);
-	}
-
 }
