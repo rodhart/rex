c3f39dc0c600afa3ac4054265276214d6e7dfd53
siegel <siegel@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-05-12 17:46:41 +0000

    Some corrections to script.   /tmp didn't work because ran out of space on device.

--- trunk/scripts/autotest.pl
+++ trunk/scripts/autotest.pl
@@ -2,7 +2,7 @@
 
 # Perl script to do automatic testing and coverage analysis of REX project
 
-$home = "/tmp";
+$home = "/usa/siegel/tmp";
 $ant = "/usr/local/ant/bin/ant";
 $svn = "/usr/local/bin/svn";
 $svndigest = "/usr/local/bin/svndigest";
@@ -26,7 +26,7 @@ chdir("$home/$maindir") || die "Could not chdir $home/maindir";
 $cmd = "$svndigest";
 system("$cmd >>$out 2>$err");
 system("rm -rf /home/www/cisc475/htdocs/rex-digest");
-system("cp -r rex-digest /home/www/cisc475/htdocs/");
+system("cp -r rex-test /home/www/cisc475/htdocs/rex-digest");
 
 system("cp build-configs/build.properties-cisc475 build.properties");
 
