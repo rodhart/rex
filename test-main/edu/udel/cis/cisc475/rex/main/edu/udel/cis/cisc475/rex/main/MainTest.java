7cab5613c1235e8c6aac98dbaae00042123029b3
mcloughl <mcloughl@2efc9e58-a604-4bd0-7c05-b9489738dcc2>
2010-05-18 02:44:06 +0000

    Added more coverage to JUnit tests.

--- test-main/edu/udel/cis/cisc475/rex/main/edu/udel/cis/cisc475/rex/main/MainTest.java
+++ test-main/edu/udel/cis/cisc475/rex/main/edu/udel/cis/cisc475/rex/main/MainTest.java
@@ -10,6 +10,34 @@ public class MainTest {
 
 	Rex.main(args);	
 	}
-	
-	
+	@Test
+	public void SeedRunCompletionTest(){
+	String [] args = {"-seed","234","examples/exam_cisc106.tex","examples/exam_cisc106.ecf"};
+
+	Rex.main(args);	
+	}
+	@Test
+	public void NRunCompletionTest(){
+	String [] args = {"-n","3","examples/exam_cisc106.tex","examples/exam_cisc106.ecf"};
+
+	Rex.main(args);	
+	}
+	@Test
+	public void PDFRunCompletionTest(){
+	String [] args = {"-pdf","examples/exam_cisc106.tex","examples/exam_cisc106.ecf"};
+
+	Rex.main(args);	
+	}
+	@Test
+	public void BadArgsTest(){
+	String [] args = {"--h","examples/exam_cisc106.tex","examples/exam_cisc106.ecf"};
+
+	Rex.main(args);	
+	}
+	@Test
+	public void doubleArgsTest(){
+	String [] args = {"-seed","2","-seed","4","examples/exam_cisc106.tex","examples/exam_cisc106.ecf"};
+
+	Rex.main(args);	
+	}
 }
